2b72a5737f77aae947ef255abadf9fa423dc685d
Anthony MOI <m.anthony.moi@gmail.com>
2019-11-01 13:54:17 -0400

    Basic bin+lib setup

---
+++ tokenizers/src/cli.rs
@@ -0,0 +1,7 @@
+/// This is the CLI binary for the Tokenizers project
+use tokenizers::test;
+
+fn main() {
+    println!("Hello, world!");
+    test();
+}
---
+++ tokenizers/src/lib.rs
@@ -0,0 +1,3 @@
+pub fn test() {
+    println!("Test");
+}
