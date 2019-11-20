351d526e1ecbb0fb0cfda63fe15c387f1695478f
Anthony MOI <m.anthony.moi@gmail.com>
2019-11-19 19:31:33 -0500

    Basic python bindings

--- bindings/python/example.py
+++ bindings/python/example.py
@@ -1,3 +1,71 @@
-from tokenizers import WhitespaceTokenizer
+import os
+import time
+import argparse
 
-print(WhitespaceTokenizer.tokenize("Hey man!"))
+from tokenizers import Tokenizer
+from transformers import GPT2Tokenizer
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", default=None, type=str, help="The file to encode")
+args = parser.parse_args()
+
+if args.file is not None:
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(current_dir, args.file)
+
+    with open(path, "r") as fp:
+        text = [ line.strip() for line in fp ]
+else:
+    text = """
+    The Zen of Python, by Tim Peters
+    Beautiful is better than ugly.
+    Explicit is better than implicit.
+    Simple is better than complex.
+    Complex is better than complicated.
+    Flat is better than nested.
+    Sparse is better than dense.
+    Readability counts.
+    Special cases aren't special enough to break the rules.
+    Although practicality beats purity.
+    Errors should never pass silently.
+    Unless explicitly silenced.
+    In the face of ambiguity, refuse the temptation to guess.
+    There should be one-- and preferably only one --obvious way to do it.
+    Although that way may not be obvious at first unless you're Dutch.
+    Now is better than never.
+    Although never is often better than *right* now.
+    If the implementation is hard to explain, it's a bad idea.
+    If the implementation is easy to explain, it may be a good idea.
+    Namespaces are one honking great idea -- let's do more of those!
+    """.split("\n")
+
+
+tok_p = GPT2Tokenizer.from_pretrained('gpt2')
+tok_r = Tokenizer.bpe_from_files(
+    "../../data/gpt2-vocab.json",
+    "../../data/gpt2-merges.txt",
+    pre_tokenizer="ByteLevel",
+)
+
+def tokenize_r():
+    # return [ tok_r.encode(sentence) for sentence in text]
+    return tok_r.encode_batch(text);
+
+def tokenize_p():
+    return [tok_p.encode(sentence) for sentence in text]
+
+print(f"Tokenizing {len(text)} lines")
+
+# Rust version
+start = time.time()
+encoded_r = tokenize_r()
+end = time.time()
+print(f"Rust tokenizer took: {end - start} sec")
+
+# Python version
+start = time.time()
+encoded_p = tokenize_p()
+end = time.time()
+print(f"Transformer tokenizer took: {end - start} sec")
+
+assert([ [ token.id for token in sentence] for sentence in encoded_r ] == encoded_p)
--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,20 +1,162 @@
 extern crate tokenizers as tk;
+use tk::models::bpe::Error as BpeError;
 
+use pyo3::exceptions;
 use pyo3::prelude::*;
+use pyo3::types::{PyDict, PyList};
 
 #[pyclass]
-struct WhitespaceTokenizer {}
+#[repr(transparent)]
+struct Token {
+    tok: tk::tokenizer::Token,
+}
+impl Token {
+    pub fn new(tok: tk::tokenizer::Token) -> Self {
+        Token { tok }
+    }
+}
 
 #[pymethods]
-impl WhitespaceTokenizer {
+impl Token {
+    #[getter]
+    fn get_id(&self) -> PyResult<u32> {
+        Ok(self.tok.id)
+    }
+
+    #[getter]
+    fn get_value(&self) -> PyResult<&str> {
+        Ok(&self.tok.value)
+    }
+
+    #[getter]
+    fn get_offsets(&self) -> PyResult<(usize, usize)> {
+        Ok(self.tok.offsets)
+    }
+
+    fn as_tuple(&self) -> PyResult<(u32, &str, (usize, usize))> {
+        Ok((self.tok.id, &self.tok.value, self.tok.offsets))
+    }
+}
+
+fn get_pre_tokenizer(name: &str) -> Option<Box<dyn tk::tokenizer::PreTokenizer + Sync>> {
+    match name {
+        "ByteLevel" => Some(Box::new(tk::pre_tokenizers::byte_level::ByteLevel)),
+        "Whitespace" => Some(Box::new(tk::pre_tokenizers::whitespace::Whitespace)),
+        _ => None,
+    }
+}
+
+fn get_normalizer(_name: &str) -> Option<Box<dyn tk::tokenizer::Normalizer + Sync>> {
+    None
+}
+
+fn get_post_processor(_name: &str) -> Option<Box<dyn tk::tokenizer::PostProcessor + Sync>> {
+    None
+}
+
+#[pyclass]
+struct Tokenizer {
+    tokenizer: tk::tokenizer::Tokenizer,
+}
+#[pymethods]
+impl Tokenizer {
     #[staticmethod]
-    fn tokenize(s: String) -> PyResult<Vec<String>> {
-        Ok(tk::WhitespaceTokenizer::tokenize(&s))
+    #[args(kwargs = "**")]
+    fn bpe_from_files(vocab: &str, merges: &str, kwargs: Option<&PyDict>) -> PyResult<Self> {
+        let model = match tk::models::bpe::BPE::from_files(vocab, merges) {
+            Ok(bpe) => Ok(Box::new(bpe)),
+            Err(e) => match e {
+                BpeError::BadVocabulary => {
+                    Err(exceptions::Exception::py_err("Bad vocab.json format"))
+                }
+                BpeError::Io(io) => Err(PyErr::from(io)),
+                BpeError::JsonError(_) => Err(exceptions::Exception::py_err(
+                    "Error while parsing vocab json file",
+                )),
+                BpeError::MergeTokenOutOfVocabulary(token) => Err(exceptions::Exception::py_err(
+                    format!("Merge token out of vocabulary: {}", token),
+                )),
+            },
+        }?;
+
+        let mut tokenizer = tk::tokenizer::Tokenizer::new(model);
+
+        if let Some(kwargs) = kwargs {
+            for (option, value) in kwargs {
+                match option.to_string().as_ref() {
+                    "pre_tokenizer" => {
+                        let value = value.to_string();
+                        if let Some(pre_tokenizer) = get_pre_tokenizer(&value) {
+                            tokenizer.with_pre_tokenizer(pre_tokenizer);
+                        } else {
+                            return Err(exceptions::Exception::py_err(format!(
+                                "PreTokenizer `{}` not found",
+                                value
+                            )));
+                        }
+                    }
+                    "normalizers" => {
+                        let mut normalizers = vec![];
+                        let values = value.cast_as::<PyList>()?;
+                        for value in values {
+                            let value = value.to_string();
+                            if let Some(normalizer) = get_normalizer(&value) {
+                                normalizers.push(normalizer);
+                            } else {
+                                return Err(exceptions::Exception::py_err(format!(
+                                    "Normalizer `{}` not found",
+                                    value
+                                )));
+                            }
+                        }
+                    }
+                    "post_processors" => {
+                        let mut processors = vec![];
+                        let values = value.cast_as::<PyList>()?;
+                        for value in values {
+                            let value = value.to_string();
+                            if let Some(processor) = get_post_processor(&value) {
+                                processors.push(processor);
+                            } else {
+                                return Err(exceptions::Exception::py_err(format!(
+                                    "PostProcessor `{}` not found",
+                                    value
+                                )));
+                            }
+                        }
+                    }
+                    _ => println!("Ignored unknown kwarg `{}`", option),
+                }
+            }
+        }
+
+        Ok(Tokenizer { tokenizer })
+    }
+
+    fn encode(&self, sentence: &str) -> Vec<Token> {
+        self.tokenizer
+            .encode(sentence)
+            .into_iter()
+            .map(|token| Token::new(token))
+            .collect()
+    }
+
+    fn encode_batch(&self, sentences: Vec<&str>) -> Vec<Vec<Token>> {
+        self.tokenizer
+            .encode_batch(sentences)
+            .into_iter()
+            .map(|sentence| {
+                sentence
+                    .into_iter()
+                    .map(|token| Token::new(token))
+                    .collect()
+            })
+            .collect()
     }
 }
 
 #[pymodule]
-fn tokenizers(py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<WhitespaceTokenizer>()?;
+fn tokenizers(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<Tokenizer>()?;
     Ok(())
 }
