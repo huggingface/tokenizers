d565bbf3096864e5d04b3fbeb0ca6529b4a04375
Anthony MOI <m.anthony.moi@gmail.com>
2019-11-26 17:51:26 -0500

    Container - Add ability to execute

--- bindings/python/src/utils.rs
+++ bindings/python/src/utils.rs
@@ -40,4 +40,20 @@ where
             None
         }
     }
+
+    pub fn execute<F, U>(&self, closure: F) -> U
+    where
+        F: FnOnce(&Box<T>) -> U,
+    {
+        match self {
+            Container::Owned(val) => closure(val),
+            Container::Pointer(ptr) => unsafe {
+                let val = Box::from_raw(*ptr);
+                let res = closure(&val);
+                // We call this to make sure we don't drop the Box
+                Box::into_raw(val);
+                res
+            },
+        }
+    }
 }
