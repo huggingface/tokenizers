5db08ac15dd438baf4bebbae49569768755e6d60
Anthony MOI <m.anthony.moi@gmail.com>
2019-11-29 22:36:17 -0500

    Update wheel building

---
+++ bindings/python/build-wheels.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly-2019-11-01 -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
+cd /io/bindings/python
+
+for PYBIN in /opt/python/{cp35-cp35m,cp36-cp36m,cp37-cp37m}/bin; do
+    export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
+
+    "${PYBIN}/pip" install -U setuptools-rust
+    "${PYBIN}/python" setup.py bdist_wheel
+done
+
+for whl in dist/*.whl; do
+    auditwheel repair "$whl" -w dist/
+done
--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -4,6 +4,11 @@ from setuptools_rust import Binding, RustExtension
 setup(
     name="tokenizers",
     version="0.0.2",
+    description="Fast and Customizable Tokenizers",
+    author="Anthony MOI",
+    author_email="anthony@huggingface.co",
+    url="https://github.com/huggingface/tokenizers",
+    license="Apache License 2.0",
     rust_extensions=[RustExtension("tokenizers.tokenizers", binding=Binding.PyO3)],
     packages=["tokenizers"],
     zip_safe=False,
