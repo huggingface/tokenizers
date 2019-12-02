9e564844071701c36536c95306adcd4551bb64cc
Anthony MOI <m.anthony.moi@gmail.com>
2019-12-02 18:24:26 -0500

    Build also for python 3.8

--- bindings/python/build-wheels.sh
+++ bindings/python/build-wheels.sh
@@ -4,7 +4,7 @@ set -ex
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly-2019-11-01 -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
-for PYBIN in /opt/python/{cp35-cp35m,cp36-cp36m,cp37-cp37m}/bin; do
+for PYBIN in /opt/python/{cp35-cp35m,cp36-cp36m,cp37-cp37m,cp38-cp38}/bin; do
     export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
 
     "${PYBIN}/pip" install -U setuptools-rust
