3abdfaf852d39c731aaab3308c428a21c8b1b823
Anthony MOI <m.anthony.moi@gmail.com>
2019-12-06 19:30:16 -0500

    Add WordPiece model for bert

--- tokenizers/src/models/mod.rs
+++ tokenizers/src/models/mod.rs
@@ -1,1 +1,2 @@
 pub mod bpe;
+pub mod wordpiece;
---
+++ tokenizers/src/models/wordpiece/mod.rs
@@ -0,0 +1,131 @@
+use crate::tokenizer::{Model, Token};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+pub struct WordPiece {
+    unk_token: String,
+    max_input_chars_per_word: usize,
+    vocab: HashMap<String, u32>,
+    vocab_r: HashMap<u32, String>,
+}
+
+impl Default for WordPiece {
+    fn default() -> Self {
+        WordPiece {
+            vocab: HashMap::new(),
+            vocab_r: HashMap::new(),
+            unk_token: String::from("[UNK]"),
+            max_input_chars_per_word: 100,
+        }
+    }
+}
+
+impl WordPiece {
+    pub fn from_files(
+        vocab: &str,
+        unk_token: String,
+        max_input_chars_per_word: Option<usize>,
+    ) -> std::io::Result<Self> {
+        let file = File::open(vocab)?;
+        let file = BufReader::new(file);
+
+        let mut vocab = HashMap::new();
+        for (index, line) in file.lines().enumerate() {
+            let line = line?;
+            vocab.insert(line.trim_end().to_owned(), index as u32);
+        }
+
+        Ok(WordPiece {
+            vocab: vocab.clone(),
+            vocab_r: vocab.into_iter().map(|(token, id)| (id, token)).collect(),
+            unk_token,
+            max_input_chars_per_word: max_input_chars_per_word.unwrap_or(100),
+        })
+    }
+}
+
+impl Model for WordPiece {
+    fn tokenize(&self, sentence: Vec<String>) -> Vec<Token> {
+        let mut output_tokens = vec![];
+
+        let mut offset = 0usize;
+        for token in sentence {
+            let char_len = token.chars().count();
+            if char_len > self.max_input_chars_per_word {
+                output_tokens.push(Token {
+                    value: self.unk_token.clone(),
+                    id: *self.vocab.get(&self.unk_token).unwrap_or(&0),
+                    offsets: (offset, offset + char_len),
+                });
+                continue;
+            }
+
+            let mut is_bad = false;
+            let mut start = 0;
+            let mut sub_tokens: Vec<Token> = vec![];
+            let chars = token.chars().collect::<Vec<_>>();
+
+            while start < chars.len() {
+                let mut end = chars.len();
+                let mut cur_str = None;
+
+                while start < end {
+                    let mut substr = chars[start..end].iter().collect::<String>();
+                    if start > 0 {
+                        substr = format!("##{}", substr);
+                    }
+                    if self.vocab.contains_key(&substr) {
+                        cur_str = Some(Token {
+                            id: self.vocab[&substr],
+                            value: substr,
+                            offsets: (offset + start, offset + end),
+                        });
+                        break;
+                    }
+                    end -= 1;
+                }
+
+                if cur_str.is_none() {
+                    is_bad = true;
+                    break;
+                }
+
+                sub_tokens.push(cur_str.unwrap());
+                start = end;
+            }
+
+            if is_bad {
+                output_tokens.push(Token {
+                    value: self.unk_token.clone(),
+                    id: *self.vocab.get(&self.unk_token).unwrap_or(&0),
+                    offsets: (offset, offset + char_len),
+                });
+            } else {
+                output_tokens.extend(sub_tokens);
+            }
+
+            offset += char_len;
+        }
+
+        output_tokens
+    }
+
+    fn decode(&self, ids: Vec<u32>) -> Vec<String> {
+        ids.into_iter()
+            .map(|id| self.vocab_r.get(&id))
+            .filter(|token| token.is_some())
+            .map(|id| id.unwrap().clone())
+            .collect()
+    }
+
+    fn token_to_id(&self, token: &str) -> Option<u32> {
+        self.vocab.get(token).map(|id| *id)
+    }
+
+    fn id_to_token(&self, id: u32) -> Option<String> {
+        self.vocab_r.get(&id).map(|token| token.clone())
+    }
+}
