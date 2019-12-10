7cb2fe2ea0b36e6a9ba74ed863865fa02b7d1f87
Anthony MOI <m.anthony.moi@gmail.com>
2019-12-10 18:01:07 -0500

    Bump version

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="tokenizers",
-    version="0.0.3",
+    version="0.0.4",
     description="Fast and Customizable Tokenizers",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
