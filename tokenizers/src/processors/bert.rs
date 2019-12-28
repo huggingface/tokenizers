be00a1e45e4d76432eb6d2cd5eea7541d9b34d24
Anthony MOI <m.anthony.moi@gmail.com>
2019-12-28 15:45:51 -0500

    Improve clarity for BertProcessing

--- tokenizers/src/processors/bert.rs
+++ tokenizers/src/processors/bert.rs
@@ -25,59 +25,52 @@ impl PostProcessor for BertProcessing {
     }
 
     fn process(&self, mut encoding: Encoding, pair_encoding: Option<Encoding>) -> Result<Encoding> {
-        // Prepare ids
         let ids = [&[self.cls.1], &encoding.get_ids()[..], &[self.sep.1]].concat();
-        let pair_ids = pair_encoding
-            .as_ref()
-            .map(|encoding| [&encoding.get_ids()[..], &[self.sep.1]].concat());
-
-        // Prepare tokens
+        let type_ids = [&[0], &encoding.get_type_ids()[..], &[0]].concat();
         let tokens = [
             &[self.cls.0.clone()],
             &encoding.get_tokens()[..],
             &[self.sep.0.clone()],
         ]
         .concat();
-        let pair_tokens = pair_encoding
-            .as_ref()
-            .map(|encoding| [&encoding.get_tokens()[..], &[self.sep.0.clone()]].concat());
-
-        // Prepare offsets
         let offsets = [&[(0, 0)], &encoding.get_offsets()[..], &[(0, 0)]].concat();
-        let pair_offsets = pair_encoding
-            .as_ref()
-            .map(|encoding| [&encoding.get_offsets()[..], &[(0, 0)]].concat());
-
-        // Prepare type ids
-        let type_ids = [&[0], &encoding.get_type_ids()[..], &[0]].concat();
-        let pair_type_ids = pair_encoding
-            .as_ref()
-            .map(|encoding| [&encoding.get_type_ids()[..], &[1]].concat());
-
         let special_tokens = [&[1u32], &vec![0; encoding.get_ids().len()][..], &[1]].concat();
-        let pair_special_tokens = pair_encoding
-            .as_ref()
-            .map(|encoding| [&vec![0u32; encoding.get_type_ids().len()][..], &[1]].concat());
+        let attention_mask = vec![1; ids.len()];
 
-        let attention_mask = vec![1; ids.len() + pair_ids.as_ref().map(|e| e.len()).unwrap_or(0)];
-
-        let normalized = encoding.get_normalized();
-        if let Some(pair) = &pair_encoding {
-            encoding.get_normalized().merge_with(pair.get_normalized());
-        };
-        Ok(Encoding::new(
-            *normalized,
-            [&ids[..], &pair_ids.unwrap_or_else(|| vec![])[..]].concat(),
-            [&type_ids[..], &pair_type_ids.unwrap_or_else(|| vec![])[..]].concat(),
-            [&tokens[..], &pair_tokens.unwrap_or_else(|| vec![])[..]].concat(),
-            [&offsets[..], &pair_offsets.unwrap_or_else(|| vec![])[..]].concat(),
-            [
-                &special_tokens[..],
-                &pair_special_tokens.unwrap_or_else(|| vec![])[..],
-            ]
-            .concat(),
+        let mut new_encoding = Encoding::new(
+            encoding.get_normalized().clone(),
+            ids,
+            type_ids,
+            tokens,
+            offsets,
+            special_tokens,
             attention_mask,
             encoding.take_overflowing(),
-        ))
+        );
+
+        if let Some(mut encoding) = pair_encoding {
+            let pair_ids = [&encoding.get_ids()[..], &[self.sep.1]].concat();
+            let pair_type_ids = [&encoding.get_type_ids()[..], &[1]].concat();
+            let pair_tokens = [&encoding.get_tokens()[..], &[self.sep.0.clone()]].concat();
+            let pair_offsets = [&encoding.get_offsets()[..], &[(0, 0)]].concat();
+            let pair_special_tokens =
+                [&vec![0u32; encoding.get_type_ids().len()][..], &[1]].concat();
+            let pair_attention_mask = vec![1; pair_ids.len()];
+
+            let new_pair_encoding = Encoding::new(
+                encoding.get_normalized().clone(),
+                pair_ids,
+                pair_type_ids,
+                pair_tokens,
+                pair_offsets,
+                pair_special_tokens,
+                pair_attention_mask,
+                encoding.take_overflowing(),
+            );
+
+            new_encoding.merge_with(new_pair_encoding);
+        }
+
+        Ok(new_encoding)
     }
 }
