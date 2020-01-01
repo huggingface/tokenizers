3594f7a9fca87221a2f4e05e3288876e1ffa6f99
Morgan Funtowicz <morgan@huggingface.co>
2020-01-01 17:55:10 +0100

    Fix invalid setup.py structure leading to build issue.
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -19,29 +19,32 @@ def get_py_version_cfgs():
 
 
 def make_rust_extension(module_name):
-    setup(
-        name="tokenizers",
-        version="0.0.11",
-        description="Fast and Customizable Tokenizers",
-        long_description=open("README.md", "r", encoding="utf-8").read(),
-        long_description_content_type="text/markdown",
-        keywords="NLP tokenizer BPE transformer deep learning",
-        author="Anthony MOI",
-        author_email="anthony@huggingface.co",
-        url="https://github.com/huggingface/tokenizers",
-        license="Apache License 2.0",
-        rust_extensions=[
-            make_rust_extension("tokenizers"),
-            make_rust_extension("tokenizers.decoders"),
-            make_rust_extension("tokenizers.encoding"),
-            make_rust_extension("tokenizers.error"),
-            make_rust_extension("tokenizers.models"),
-            make_rust_extension("tokenizers.pretokenizers"),
-            make_rust_extension("tokenizers.trainer"),
-        ],
-        packages=["tokenizers"],
-        zip_safe=False,
-    )
     return RustExtension(
         module_name, "Cargo.toml", rustc_flags=get_py_version_cfgs(), binding=Binding.PyO3, debug=None
     )
+
+
+setup(
+    name="tokenizers",
+    version="0.0.11",
+    description="Fast and Customizable Tokenizers",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="NLP tokenizer BPE transformer deep learning",
+    author="Anthony MOI",
+    author_email="anthony@huggingface.co",
+    url="https://github.com/huggingface/tokenizers",
+    license="Apache License 2.0",
+    rust_extensions=[
+        make_rust_extension("tokenizers"),
+        make_rust_extension("tokenizers.decoders"),
+        make_rust_extension("tokenizers.encoding"),
+        make_rust_extension("tokenizers.error"),
+        make_rust_extension("tokenizers.models"),
+        make_rust_extension("tokenizers.pretokenizers"),
+        make_rust_extension("tokenizers.trainer"),
+    ],
+    packages=["tokenizers"],
+    include_package_data=True,
+    zip_safe=False,
+)
