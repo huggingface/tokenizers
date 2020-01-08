c7d2800131351d2403d8cbef5390f608f9e78317
Anthony MOI <m.anthony.moi@gmail.com>
2020-01-08 14:44:17 -0500

    Python - Add model saving to base tokenizer

--- bindings/python/tokenizers/implementations/base_tokenizer.py
+++ bindings/python/tokenizers/implementations/base_tokenizer.py
@@ -188,3 +188,14 @@ class BaseTokenizer:
         """
         return self._tokenizer.id_to_token(id)
 
+    def save(self, directory: str, name: str):
+        """ Save the current model to the given directory
+
+        Args:
+            directory: str:
+                A path to the destination directory
+
+            name: str:
+                The name of the tokenizer, to be used in the saved files
+        """
+        return self._tokenizer.model.save(directory, name)
