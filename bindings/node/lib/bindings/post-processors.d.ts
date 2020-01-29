01f6a5bc15b130e717a7a90bfb5b98d6b765f268
Morgan Funtowicz <morgan@huggingface.co>
2020-01-29 17:17:37 +0100

    Attempt to make it works on Node side too.
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>

--- bindings/node/lib/bindings/post-processors.d.ts
+++ bindings/node/lib/bindings/post-processors.d.ts
@@ -15,3 +15,15 @@ export function bertProcessing(
   sep: [string, number],
   cls: [string, number]
 ): PostProcessor;
+
+
+/**
+ * Instantiate a new RobertaProcessing with the given tokens
+ *
+ * @param sep A tuple with the string representation of the SEP token, and its id
+ * @param cls A tuple with the string representation of the CLS token, and its id
+ */
+export function robertaProcessing(
+    sep: [string, number],
+    cls: [string, number]
+): PostProcessor;
