6524f09e991c3a52c839d8eb01bfa41e81fde1d1
Funtowicz Morgan <mfuntowicz@users.noreply.github.com>
2020-02-03 10:39:48 +0000

    Roberta PostProcessor (#111)
    * Added RobertaProcessor on Rust side.
    
    Required to match the double separator token in the middle of pairs.
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * Fix typo in RobertaProcessing method declaration
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * Correctly include RobertProcessor in the Python binding
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * Roberta doesnt use token_type_ids so let's set everything to 0
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * Attempt to make it works on Node side too.
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * fix js bindings / `npm run lint`
    
    * Make RustFmt happy.
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    Co-authored-by: Pierric Cistac <Pierrci@users.noreply.github.com>

--- bindings/node/native/src/processors.rs
+++ bindings/node/native/src/processors.rs
@@ -55,8 +55,45 @@ fn bert_processing(mut cx: FunctionContext) -> JsResult<JsPostProcessor> {
     Ok(processor)
 }
 
+/// roberta_processing(sep: [String, number], cls: [String, number])
+fn roberta_processing(mut cx: FunctionContext) -> JsResult<JsPostProcessor> {
+    let sep = cx.argument::<JsArray>(0)?;
+    let cls = cx.argument::<JsArray>(1)?;
+    if sep.len() != 2 || cls.len() != 2 {
+        return cx.throw_error("SEP and CLS must be of the form: [String, number]");
+    }
+    let sep: (String, u32) = (
+        sep.get(&mut cx, 0)?
+            .downcast::<JsString>()
+            .or_throw(&mut cx)?
+            .value(),
+        sep.get(&mut cx, 1)?
+            .downcast::<JsNumber>()
+            .or_throw(&mut cx)?
+            .value() as u32,
+    );
+    let cls: (String, u32) = (
+        cls.get(&mut cx, 0)?
+            .downcast::<JsString>()
+            .or_throw(&mut cx)?
+            .value(),
+        cls.get(&mut cx, 1)?
+            .downcast::<JsNumber>()
+            .or_throw(&mut cx)?
+            .value() as u32,
+    );
+
+    let mut processor = JsPostProcessor::new::<_, JsPostProcessor, _>(&mut cx, vec![])?;
+    let guard = cx.lock();
+    processor.borrow_mut(&guard).processor.to_owned(Box::new(
+        tk::processors::roberta::RobertaProcessing::new(sep, cls),
+    ));
+    Ok(processor)
+}
+
 /// Register everything here
 pub fn register(m: &mut ModuleContext, prefix: &str) -> NeonResult<()> {
     m.export_function(&format!("{}_BertProcessing", prefix), bert_processing)?;
+    m.export_function(&format!("{}_RobertaProcessing", prefix), roberta_processing)?;
     Ok(())
 }
