e365c1992ba3492fef39ce3234972c3c15f1ea81
Funtowicz Morgan <mfuntowicz@users.noreply.github.com>
2020-02-03 10:41:33 +0000

    Improve flexibility in some Python binding  (#107)
    * Fix invalid method bindings on Python side.
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * Introduce factory function to create normalizer instance from the name of an unicode normalizer.
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * Rename BPETokenizer to CharBPETokenizer for clarity
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * Give more flexibility in the way CharBPETokenizer handles normalizers creation.
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * Change .pyi file to reflection Normalizer hierarchy
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>
    
    * Make ByteLevelBPE as flexible for normalization than CharBPE.
    
    Signed-off-by: Morgan Funtowicz <morgan@huggingface.co>

--- bindings/python/tokenizers/normalizers/__init__.py
+++ bindings/python/tokenizers/normalizers/__init__.py
@@ -8,3 +8,21 @@ NFC = normalizers.NFC
 NFKC = normalizers.NFKC
 Sequence = normalizers.Sequence
 Lowercase = normalizers.Lowercase
+
+
+NORMALIZERS = {
+    "nfc": NFC,
+    "nfd": NFD,
+    "nfkc": NFKC,
+    "nfkd": NFKD
+}
+
+
+def unicode_normalizer_from_str(normalizer: str) -> Normalizer:
+    if normalizer not in NORMALIZERS:
+        raise ValueError(
+            "{} is not a known unicode normalizer. Available are {}"
+                .format(normalizer, NORMALIZERS.keys())
+        )
+
+    return NORMALIZERS[normalizer]()
