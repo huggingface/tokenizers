b132be34af618ce94b25be2c4b6d669281833ac4
Anthony MOI <m.anthony.moi@gmail.com>
2020-03-26 17:26:14 -0400

    Bump version for Python release

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name="tokenizers",
-    version="0.6.0",
+    version="0.7.0-rc1",
     description="Fast and Customizable Tokenizers",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
--- bindings/python/tokenizers/__init__.py
+++ bindings/python/tokenizers/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.6.0"
+__version__ = "0.7.0-rc1"
 
 from typing import Tuple
 
