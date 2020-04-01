837791ee1f0ee8badf0c56b7bc693acffd12c1c8
Anthony MOI <m.anthony.moi@gmail.com>
2020-04-01 15:14:36 -0400

    Python - Test BertWordPieceTokenizer

---
+++ bindings/python/tests/implementations/test_bert_wordpiece.py
@@ -0,0 +1,21 @@
+from ..utils import data_dir, bert_files
+from tokenizers import BertWordPieceTokenizer
+
+
+class TestBertWordPieceBPE:
+    def test_basic_encode(self, bert_files):
+        tokenizer = BertWordPieceTokenizer(bert_files["vocab"])
+
+        # Encode with special tokens by default
+        output = tokenizer.encode("My name is John", "pair")
+        assert output.ids == [101, 2026, 2171, 2003, 2198, 102, 3940, 102]
+        assert output.tokens == ["[CLS]", "my", "name", "is", "john", "[SEP]", "pair", "[SEP]"]
+        assert output.offsets == [(0, 0), (0, 2), (3, 7), (8, 10), (11, 15), (0, 0), (0, 4), (0, 0)]
+        assert output.type_ids == [0, 0, 0, 0, 0, 0, 1, 1]
+
+        # Can encode without the special tokens
+        output = tokenizer.encode("My name is John", "pair", add_special_tokens=False)
+        assert output.ids == [2026, 2171, 2003, 2198, 3940]
+        assert output.tokens == ["my", "name", "is", "john", "pair"]
+        assert output.offsets == [(0, 2), (3, 7), (8, 10), (11, 15), (0, 4)]
+        assert output.type_ids == [0, 0, 0, 0, 1]
