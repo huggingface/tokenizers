1b5f29adac90639085415304756468e2f69a19ec
Anthony MOI <m.anthony.moi@gmail.com>
2020-06-10 18:29:44 -0400

    Node - Improve extraction for post-processors

--- bindings/node/native/src/processors.rs
+++ bindings/node/native/src/processors.rs
@@ -1,6 +1,7 @@
 extern crate tokenizers as tk;
 
 use crate::container::Container;
+use crate::extraction::*;
 use neon::prelude::*;
 
 /// Processor
@@ -21,31 +22,8 @@ declare_types! {
 
 /// bert_processing(sep: [String, number], cls: [String, number])
 fn bert_processing(mut cx: FunctionContext) -> JsResult<JsPostProcessor> {
-    let sep = cx.argument::<JsArray>(0)?;
-    let cls = cx.argument::<JsArray>(1)?;
-    if sep.len() != 2 || cls.len() != 2 {
-        return cx.throw_error("SEP and CLS must be of the form: [String, number]");
-    }
-    let sep: (String, u32) = (
-        sep.get(&mut cx, 0)?
-            .downcast::<JsString>()
-            .or_throw(&mut cx)?
-            .value(),
-        sep.get(&mut cx, 1)?
-            .downcast::<JsNumber>()
-            .or_throw(&mut cx)?
-            .value() as u32,
-    );
-    let cls: (String, u32) = (
-        cls.get(&mut cx, 0)?
-            .downcast::<JsString>()
-            .or_throw(&mut cx)?
-            .value(),
-        cls.get(&mut cx, 1)?
-            .downcast::<JsNumber>()
-            .or_throw(&mut cx)?
-            .value() as u32,
-    );
+    let sep = cx.extract::<(String, u32)>(0)?;
+    let cls = cx.extract::<(String, u32)>(1)?;
 
     let mut processor = JsPostProcessor::new::<_, JsPostProcessor, _>(&mut cx, vec![])?;
     let guard = cx.lock();
@@ -62,44 +40,15 @@ fn bert_processing(mut cx: FunctionContext) -> JsResult<JsPostProcessor> {
 ///   addPrefixSpace: boolean = true
 /// )
 fn roberta_processing(mut cx: FunctionContext) -> JsResult<JsPostProcessor> {
-    let sep = cx.argument::<JsArray>(0)?;
-    let cls = cx.argument::<JsArray>(1)?;
-    if sep.len() != 2 || cls.len() != 2 {
-        return cx.throw_error("SEP and CLS must be of the form: [String, number]");
-    }
-    let sep: (String, u32) = (
-        sep.get(&mut cx, 0)?
-            .downcast::<JsString>()
-            .or_throw(&mut cx)?
-            .value(),
-        sep.get(&mut cx, 1)?
-            .downcast::<JsNumber>()
-            .or_throw(&mut cx)?
-            .value() as u32,
-    );
-    let cls: (String, u32) = (
-        cls.get(&mut cx, 0)?
-            .downcast::<JsString>()
-            .or_throw(&mut cx)?
-            .value(),
-        cls.get(&mut cx, 1)?
-            .downcast::<JsNumber>()
-            .or_throw(&mut cx)?
-            .value() as u32,
-    );
+    let sep = cx.extract::<(String, u32)>(0)?;
+    let cls = cx.extract::<(String, u32)>(1)?;
 
     let mut processor = tk::processors::roberta::RobertaProcessing::new(sep, cls);
-    if let Some(args) = cx.argument_opt(2) {
-        if args.downcast::<JsUndefined>().is_err() {
-            processor =
-                processor.trim_offsets(args.downcast::<JsBoolean>().or_throw(&mut cx)?.value());
-        }
+    if let Some(trim_offsets) = cx.extract_opt::<bool>(2)? {
+        processor = processor.trim_offsets(trim_offsets);
     }
-    if let Some(args) = cx.argument_opt(3) {
-        if args.downcast::<JsUndefined>().is_err() {
-            processor =
-                processor.add_prefix_space(args.downcast::<JsBoolean>().or_throw(&mut cx)?.value());
-        }
+    if let Some(add_prefix_space) = cx.extract_opt::<bool>(3)? {
+        processor = processor.add_prefix_space(add_prefix_space);
     }
 
     let mut js_processor = JsPostProcessor::new::<_, JsPostProcessor, _>(&mut cx, vec![])?;
@@ -115,11 +64,8 @@ fn roberta_processing(mut cx: FunctionContext) -> JsResult<JsPostProcessor> {
 fn bytelevel(mut cx: FunctionContext) -> JsResult<JsPostProcessor> {
     let mut byte_level = tk::processors::byte_level::ByteLevel::default();
 
-    if let Some(args) = cx.argument_opt(0) {
-        if args.downcast::<JsUndefined>().is_err() {
-            byte_level =
-                byte_level.trim_offsets(args.downcast::<JsBoolean>().or_throw(&mut cx)?.value());
-        }
+    if let Some(trim_offsets) = cx.extract_opt::<bool>(0)? {
+        byte_level = byte_level.trim_offsets(trim_offsets);
     }
 
     let mut processor = JsPostProcessor::new::<_, JsPostProcessor, _>(&mut cx, vec![])?;
