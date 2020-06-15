9d0be49b1113413a39a6344bdb6643200f8d561e
Nicolas Patry <patry.nicolas@protonmail.com>
2020-06-15 15:41:45 +0200

    Functional viterbi algorithm on the lattice.

--- tokenizers/src/models/unigram/lattice.rs
+++ tokenizers/src/models/unigram/lattice.rs
@@ -1,11 +1,15 @@
+use std::cell::{Ref, RefCell};
 use std::rc::Rc;
 use unicode_segmentation::UnicodeSegmentation;
+
+type NodeRef = Rc<RefCell<Node>>;
 
 pub struct Lattice<'a> {
     sentence: &'a str,
     graphemes: Vec<&'a str>,
-    begin_nodes: Vec<Vec<Node>>,
-    end_nodes: Vec<Vec<Node>>,
+    nodes: Vec<NodeRef>,
+    begin_nodes: Vec<Vec<NodeRef>>,
+    end_nodes: Vec<Vec<NodeRef>>,
     current_id: usize,
 }
 
@@ -14,7 +18,7 @@ pub struct Node {
     id: usize,
     pos: usize,
     length: usize,
-    prev: Option<Rc<Node>>,
+    prev: Option<NodeRef>,
     backtrace_score: f64,
     score: f64,
 }
@@ -25,16 +29,14 @@ impl PartialEq for Node {
     }
 }
 
-static mut CURRENT_ID: usize = 0;
-
 impl Node {
-    pub fn new(id: usize, pos: usize, length: usize) -> Node {
+    pub fn new(id: usize, pos: usize, length: usize, score: f64) -> Node {
         Node {
             id,
             pos,
             length,
             prev: None,
-            score: 0.0,
+            score,
             backtrace_score: 0.0,
         }
     }
@@ -50,54 +52,67 @@ impl<'a> Lattice<'a> {
         let k_reserved_node_size = 16;
         // We are adding 2 tokens, bos and eos
         let len = graphemes.len();
+        let mut nodes: Vec<NodeRef> = Vec::with_capacity(k_reserved_node_size);
         let mut begin_nodes = vec![Vec::with_capacity(k_reserved_node_size); len + 1];
         let mut end_nodes = vec![Vec::with_capacity(k_reserved_node_size); len + 1];
 
-        let bos = Node::new(0, 0, 0);
-        let eos = Node::new(1, len, 0);
-        begin_nodes[len].push(eos);
-        end_nodes[0].push(bos);
+        let bos = Rc::new(RefCell::new(Node::new(0, 0, 0, 0.0)));
+        let eos = Rc::new(RefCell::new(Node::new(1, len, 0, 0.0)));
+
+        begin_nodes[len].push(Rc::clone(&eos));
+        end_nodes[0].push(Rc::clone(&bos));
+
+        nodes.push(bos);
+        nodes.push(eos);
 
         let current_id = 2;
         Lattice {
             sentence,
             graphemes,
+            nodes,
             begin_nodes,
             end_nodes,
             current_id,
         }
     }
 
-    pub fn insert(&mut self, pos: usize, length: usize) -> Node {
-        let node = Node::new(self.current_id, pos, length);
-        self.current_id += 1;
+    pub fn insert(&mut self, pos: usize, length: usize, score: f64) {
+        let node = Rc::new(RefCell::new(Node::new(self.current_id, pos, length, score)));
+        let res = Rc::clone(&node);
+
         // TODO node.piece ? Which is self.grapheme[pos..pos + length]
         // XXX: Careful, in sentence piece, length is in bytes, here we assume
         // it's in graphemes already, let's see if we can get away with it.
-        self.begin_nodes[pos].push(node.clone());
-        self.end_nodes[pos + length].push(node.clone());
-        node
+        self.begin_nodes[pos].push(Rc::clone(&node));
+        self.end_nodes[pos + length].push(Rc::clone(&node));
+
+        self.nodes.push(node);
+
+        self.current_id += 1;
+        // res.clone().borrow()
     }
 
-    pub fn viterbi(&mut self) -> Vec<Node> {
+    pub fn viterbi(&mut self) -> Vec<NodeRef> {
         //TODO Remove this mut it's probably unnecessary
         //  const int len = size();
         let len = self.graphemes.len();
-        for pos in 0..len {
+        for pos in 0..=len {
             // println!("Pos {:?}", pos);
             // println!("n {:?}", self.begin_nodes[pos]);
             if self.begin_nodes[pos].is_empty() {
+                println!("Empty");
                 return vec![];
             }
-            for rnode in self.begin_nodes[pos].iter_mut() {
+            for rnode in &self.begin_nodes[pos] {
                 // ??
                 // rnode->prev = nullptr;
                 // println!("Node {:?}", rnode);
-                rnode.prev = None;
+                rnode.borrow_mut().prev = None;
                 let mut best_score = 0.0;
-                let mut best_node: Option<Node> = None;
+                let mut best_node: Option<NodeRef> = None;
+                // println!("End nodes {:?}", self.end_nodes[pos]);
                 for lnode in &self.end_nodes[pos] {
-                    let score = lnode.backtrace_score + rnode.score;
+                    let score = lnode.borrow().backtrace_score + rnode.borrow().score;
                     if best_node.is_none() || score > best_score {
                         // TODO can we remove this clone ?
                         best_node = Some(lnode.clone());
@@ -107,28 +122,39 @@ impl<'a> Lattice<'a> {
                 // println!("Best node {:?}", best_node);
                 match best_node {
                     Some(bnode) => {
-                        rnode.prev = Some(Rc::new(bnode));
-                        rnode.backtrace_score = best_score;
+                        rnode.borrow_mut().prev = Some(Rc::clone(&bnode));
+                        rnode.borrow_mut().backtrace_score = best_score;
                     }
                     None => return vec![],
                 }
+                // println!("r node {:?}", rnode);
             }
         }
         // println!("Here");
 
-        let mut results: Vec<Node> = vec![];
-        println!("prev {:?}", self.begin_nodes[len]);
-        let prev = (&self.begin_nodes[len][0].prev).as_ref();
+        let mut results: Vec<NodeRef> = vec![];
+        // println!("prev {:?}", self.begin_nodes[len]);
+        let root = self.begin_nodes[len][0].borrow();
+        let prev = root.prev.as_ref();
         if prev.is_none() {
+            // println!("NOOONE");
             return vec![];
         }
-        let mut node: Rc<Node> = prev.unwrap().clone();
-        while !node.prev.is_none() {
-            results.push(Rc::make_mut(&mut node).clone());
-            node = node.prev.as_ref().unwrap().clone();
+        let mut node: NodeRef = prev.unwrap().clone();
+        while !node.borrow().prev.is_none() {
+            results.push(node.clone());
+            let n = node.borrow().clone();
+            node = n.prev.as_ref().unwrap().clone();
         }
         results.reverse();
         results
+    }
+
+    fn tokens(&mut self) -> Vec<String> {
+        self.viterbi()
+            .iter()
+            .map(|node| piece(self, &node.borrow()))
+            .collect()
     }
 
     pub fn len(&self) -> usize {
@@ -138,11 +164,11 @@ impl<'a> Lattice<'a> {
         self.sentence.len()
     }
 
-    pub fn bos_node(&self) -> &Node {
-        &self.end_nodes[0][0]
+    pub fn bos_node(&self) -> Ref<Node> {
+        self.end_nodes[0][0].borrow()
     }
-    pub fn eos_node(&self) -> &Node {
-        &self.begin_nodes[self.graphemes.len()][0]
+    pub fn eos_node(&self) -> Ref<Node> {
+        self.begin_nodes[self.graphemes.len()][0].borrow()
     }
 
     pub fn surface(&self, n: usize) -> &str {
@@ -191,8 +217,8 @@ mod tests {
 
         assert_eq!(bos.id, 0);
         assert_eq!(eos.id, 1);
-        assert_eq!(lattice.end_nodes[0].first().unwrap().id, bos.id);
-        assert_eq!(lattice.begin_nodes[4].first().unwrap().id, eos.id);
+        assert_eq!(lattice.end_nodes[0].first().unwrap().borrow().id, bos.id);
+        assert_eq!(lattice.begin_nodes[4].first().unwrap().borrow().id, eos.id);
 
         let lattice = Lattice::from("テストab");
         assert_eq!(lattice.len(), 5);
@@ -210,13 +236,20 @@ mod tests {
         let mut lattice = Lattice::from("ABあい");
         let nodes: Vec<Node> = vec![];
 
-        let node0 = lattice.insert(0, 1);
-        let node1 = lattice.insert(1, 1);
-        let node2 = lattice.insert(2, 1);
-        let node3 = lattice.insert(3, 1);
-        let node4 = lattice.insert(0, 2);
-        let node5 = lattice.insert(1, 2);
-        let node6 = lattice.insert(2, 2);
+        lattice.insert(0, 1, 0.0);
+        lattice.insert(1, 1, 0.0);
+        lattice.insert(2, 1, 0.0);
+        lattice.insert(3, 1, 0.0);
+        lattice.insert(0, 2, 0.0);
+        lattice.insert(1, 2, 0.0);
+        lattice.insert(2, 2, 0.0);
+        let node0 = lattice.nodes[2].borrow();
+        let node1 = lattice.nodes[3].borrow();
+        let node2 = lattice.nodes[4].borrow();
+        let node3 = lattice.nodes[5].borrow();
+        let node4 = lattice.nodes[6].borrow();
+        let node5 = lattice.nodes[7].borrow();
+        let node6 = lattice.nodes[8].borrow();
 
         assert_eq!(piece(&lattice, &node0), "A");
         assert_eq!(piece(&lattice, &node1), "B");
@@ -264,23 +297,23 @@ mod tests {
         assert_eq!(lattice.end_nodes[3].len(), 2);
         assert_eq!(lattice.end_nodes[4].len(), 2);
 
-        assert_eq!(lattice.begin_nodes[0][0].id, node0.id);
-        assert_eq!(lattice.begin_nodes[0][1].id, node4.id);
-        assert_eq!(lattice.begin_nodes[1][0].id, node1.id);
-        assert_eq!(lattice.begin_nodes[1][1].id, node5.id);
-        assert_eq!(lattice.begin_nodes[2][0].id, node2.id);
-        assert_eq!(lattice.begin_nodes[2][1].id, node6.id);
-        assert_eq!(lattice.begin_nodes[3][0].id, node3.id);
-        assert_eq!(lattice.eos_node().id, lattice.begin_nodes[4][0].id);
+        assert_eq!(lattice.begin_nodes[0][0].borrow().id, node0.id);
+        assert_eq!(lattice.begin_nodes[0][1].borrow().id, node4.id);
+        assert_eq!(lattice.begin_nodes[1][0].borrow().id, node1.id);
+        assert_eq!(lattice.begin_nodes[1][1].borrow().id, node5.id);
+        assert_eq!(lattice.begin_nodes[2][0].borrow().id, node2.id);
+        assert_eq!(lattice.begin_nodes[2][1].borrow().id, node6.id);
+        assert_eq!(lattice.begin_nodes[3][0].borrow().id, node3.id);
+        assert_eq!(lattice.eos_node().id, lattice.begin_nodes[4][0].borrow().id);
 
-        assert_eq!(lattice.bos_node().id, lattice.end_nodes[0][0].id);
-        assert_eq!(node0.id, lattice.end_nodes[1][0].id);
-        assert_eq!(node1.id, lattice.end_nodes[2][0].id);
-        assert_eq!(node4.id, lattice.end_nodes[2][1].id);
-        assert_eq!(node2.id, lattice.end_nodes[3][0].id);
-        assert_eq!(node5.id, lattice.end_nodes[3][1].id);
-        assert_eq!(node3.id, lattice.end_nodes[4][0].id);
-        assert_eq!(node6.id, lattice.end_nodes[4][1].id);
+        assert_eq!(lattice.bos_node().id, lattice.end_nodes[0][0].borrow().id);
+        assert_eq!(node0.id, lattice.end_nodes[1][0].borrow().id);
+        assert_eq!(node1.id, lattice.end_nodes[2][0].borrow().id);
+        assert_eq!(node4.id, lattice.end_nodes[2][1].borrow().id);
+        assert_eq!(node2.id, lattice.end_nodes[3][0].borrow().id);
+        assert_eq!(node5.id, lattice.end_nodes[3][1].borrow().id);
+        assert_eq!(node3.id, lattice.end_nodes[4][0].borrow().id);
+        assert_eq!(node6.id, lattice.end_nodes[4][1].borrow().id);
     }
 
     #[test]
@@ -288,11 +321,31 @@ mod tests {
         let mut lattice = Lattice::from("ABC");
         assert_eq!(lattice.viterbi(), vec![]);
         // Still incomplete
-        lattice.insert(0, 1);
+        lattice.insert(0, 1, 0.0);
         assert_eq!(lattice.viterbi(), vec![]);
-        lattice.insert(1, 1);
-        lattice.insert(2, 1);
+        lattice.insert(1, 1, 0.0);
+        lattice.insert(2, 1, 0.0);
         // XXX: In sentence piece this is not tested, still incomplete ?
-        // assert_eq!(lattice.viterbi(), vec![]);
+        assert_eq!(lattice.viterbi().len(), 3);
+    }
+
+    #[test]
+    fn test_viterbi2() {
+        let mut lattice = Lattice::from("ABC");
+
+        lattice.insert(0, 1, 0.0);
+        lattice.insert(1, 1, 0.0);
+        lattice.insert(2, 1, 0.0);
+
+        assert_eq!(lattice.tokens(), ["A", "B", "C"]);
+
+        lattice.insert(0, 2, 2.0);
+        assert_eq!(lattice.tokens(), ["AB", "C"]);
+
+        lattice.insert(1, 2, 5.0);
+        assert_eq!(lattice.tokens(), ["A", "BC"]);
+
+        lattice.insert(0, 3, 10.0);
+        assert_eq!(lattice.tokens(), ["ABC"]);
     }
 }
