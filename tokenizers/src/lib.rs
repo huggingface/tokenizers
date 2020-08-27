6d8955d51b96fc8db4f928f36d346c581f4c89fd
ropottnik <simonertl.1386@gmail.com>
2020-08-27 22:52:48 +0200

    Generate Readme from testable examples + template

--- tokenizers/src/lib.rs
+++ tokenizers/src/lib.rs
@@ -2,15 +2,13 @@
 #![doc(html_favicon_url = "https://huggingface.co/favicon.ico")]
 #![doc(html_logo_url = "https://huggingface.co/landing/assets/huggingface_logo.svg")]
 
-//! # Tokenizers
-//!
+//! The core of `tokenizers`, written in Rust.
 //! Provides an implementation of today's most used tokenizers, with a focus on performance and
 //! versatility.
 //!
-//! ## What is a Tokenizer
+//! # What is a Tokenizer
 //!
-//! A Tokenizer works as a pipeline, it processes some raw text as input and outputs an
-//! `Encoding`.
+//! A Tokenizer works as a pipeline, it processes some raw text as input and outputs an `Encoding`.
 //! The various steps of the pipeline are:
 //!
 //! 1. The `Normalizer`: in charge of normalizing the text. Common examples of normalization are
@@ -95,6 +93,13 @@
 //!     Ok(())
 //! }
 //! ```
+//!
+//! # Additional information
+//!
+//! - tokenizers is designed to leverage CPU parallelism when possible. The level of parallelism is determined
+//! by the total number of core/threads your CPU provides but this can be tuned by setting the `RAYON_RS_NUM_CPUS`
+//! environment variable. As an example setting `RAYON_RS_NUM_CPUS=4` will allocate a maximum of 4 threads.
+//! **_Please note this behavior may evolve in the future_**
 
 #[macro_use]
 extern crate log;
