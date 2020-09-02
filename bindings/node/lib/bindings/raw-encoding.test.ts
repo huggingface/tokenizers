90743e9298994c7743826d93d5e04f7ee942d62e
Nicolas Patry <patry.nicolas@protonmail.com>
2020-09-02 10:09:43 +0200

    Make style.

--- bindings/node/lib/bindings/raw-encoding.test.ts
+++ bindings/node/lib/bindings/raw-encoding.test.ts
@@ -3,9 +3,9 @@ import { promisify } from "util";
 import { PaddingDirection } from "./enums";
 import { Model, WordPiece, WordPieceOptions } from "./models";
 import {
-    whitespacePreTokenizer,
-    sequencePreTokenizer,
-    punctuationPreTokenizer
+  punctuationPreTokenizer,
+  sequencePreTokenizer,
+  whitespacePreTokenizer,
 } from "./pre-tokenizers";
 import { RawEncoding } from "./raw-encoding";
 import { EncodeOptions, InputSequence, Tokenizer } from "./tokenizer";
@@ -13,202 +13,204 @@ import { EncodeOptions, InputSequence, Tokenizer } from "./tokenizer";
 const MOCKS_DIR = __dirname + "/__mocks__";
 
 describe("Encoding", () => {
-    let encoding: RawEncoding;
-    let encode: (
-        sequence: InputSequence,
-        pair?: InputSequence | null,
-        options?: EncodeOptions | null
-    ) => Promise<RawEncoding>;
-    let tokenizer: Tokenizer;
+  let encoding: RawEncoding;
+  let encode: (
+    sequence: InputSequence,
+    pair?: InputSequence | null,
+    options?: EncodeOptions | null
+  ) => Promise<RawEncoding>;
+  let tokenizer: Tokenizer;
 
-    beforeAll(async () => {
-        const model = await promisify<string, WordPieceOptions, Model>(
-            WordPiece.fromFiles
-        )(`${MOCKS_DIR}/vocab.txt`, {
-            continuingSubwordPrefix: "##"
-        });
+  beforeAll(async () => {
+    const model = await promisify<string, WordPieceOptions, Model>(WordPiece.fromFiles)(
+      `${MOCKS_DIR}/vocab.txt`,
+      {
+        continuingSubwordPrefix: "##",
+      }
+    );
 
-        tokenizer = new Tokenizer(model);
-        tokenizer.setPreTokenizer(whitespacePreTokenizer());
-        encode = promisify(tokenizer.encode.bind(tokenizer));
-    });
+    tokenizer = new Tokenizer(model);
+    tokenizer.setPreTokenizer(whitespacePreTokenizer());
+    encode = promisify(tokenizer.encode.bind(tokenizer));
+  });
 
-    it("Encodes correctly", async () => {
-        encoding = await encode("my name is john", null);
-        expect(encoding.getIds()).toEqual([0, 1, 2, 3, 4]);
+  it("Encodes correctly", async () => {
+    encoding = await encode("my name is john", null);
+    expect(encoding.getIds()).toEqual([0, 1, 2, 3, 4]);
 
-        encoding = await encode("my  name is john", null);
-        expect(encoding.getIds()).toEqual([0, 1, 2, 3, 4]);
-    });
+    encoding = await encode("my  name is john", null);
+    expect(encoding.getIds()).toEqual([0, 1, 2, 3, 4]);
+  });
 
-    it("Can change pre tokenizer", async () => {
-        tokenizer.setPreTokenizer(sequencePreTokenizer([whitespacePreTokenizer()]));
+  it("Can change pre tokenizer", async () => {
+    tokenizer.setPreTokenizer(sequencePreTokenizer([whitespacePreTokenizer()]));
 
-        encoding = await encode("my  name is john.!?", null);
-        expect(encoding.getIds()).toEqual([0, 1, 2, 3, 4, 6]);
-        tokenizer.setPreTokenizer(
-            sequencePreTokenizer([whitespacePreTokenizer(), punctuationPreTokenizer()])
-        );
+    encoding = await encode("my  name is john.!?", null);
+    expect(encoding.getIds()).toEqual([0, 1, 2, 3, 4, 6]);
+    tokenizer.setPreTokenizer(
+      sequencePreTokenizer([whitespacePreTokenizer(), punctuationPreTokenizer()])
+    );
 
-        encoding = await encode("my  name is john.!?", null);
-        expect(encoding.getIds()).toEqual([0, 1, 2, 3, 4, 6, 6, 6]);
-    });
+    encoding = await encode("my  name is john.!?", null);
+    expect(encoding.getIds()).toEqual([0, 1, 2, 3, 4, 6, 6, 6]);
+  });
 });
 
 describe("RawEncoding", () => {
-    const originalString = "my name is john";
-    let encoding: RawEncoding;
-    let encode: (
-        sequence: InputSequence,
-        pair?: InputSequence | null,
-        options?: EncodeOptions | null
-    ) => Promise<RawEncoding>;
+  const originalString = "my name is john";
+  let encoding: RawEncoding;
+  let encode: (
+    sequence: InputSequence,
+    pair?: InputSequence | null,
+    options?: EncodeOptions | null
+  ) => Promise<RawEncoding>;
 
-    beforeAll(async () => {
-        const model = await promisify<string, WordPieceOptions, Model>(
-            WordPiece.fromFiles
-        )(`${MOCKS_DIR}/vocab.txt`, {
-            continuingSubwordPrefix: "##"
-        });
+  beforeAll(async () => {
+    const model = await promisify<string, WordPieceOptions, Model>(WordPiece.fromFiles)(
+      `${MOCKS_DIR}/vocab.txt`,
+      {
+        continuingSubwordPrefix: "##",
+      }
+    );
 
-        const tokenizer = new Tokenizer(model);
-        tokenizer.setPreTokenizer(whitespacePreTokenizer());
-        encode = promisify(tokenizer.encode.bind(tokenizer));
+    const tokenizer = new Tokenizer(model);
+    tokenizer.setPreTokenizer(whitespacePreTokenizer());
+    encode = promisify(tokenizer.encode.bind(tokenizer));
+  });
+
+  beforeEach(async () => {
+    encoding = await encode(originalString, null);
+  });
+
+  it("has a list of defined methods", async () => {
+    expect(typeof encoding.wordToTokens).toBe("function");
+    expect(typeof encoding.wordToChars).toBe("function");
+    expect(typeof encoding.tokenToChars).toBe("function");
+    expect(typeof encoding.tokenToWord).toBe("function");
+    expect(typeof encoding.charToToken).toBe("function");
+    expect(typeof encoding.charToWord).toBe("function");
+    expect(typeof encoding.getAttentionMask).toBe("function");
+    expect(typeof encoding.getIds).toBe("function");
+    expect(typeof encoding.getLength).toBe("function");
+    expect(typeof encoding.getOffsets).toBe("function");
+    expect(typeof encoding.getOverflowing).toBe("function");
+    expect(typeof encoding.getSpecialTokensMask).toBe("function");
+    expect(typeof encoding.getTokens).toBe("function");
+    expect(typeof encoding.getTypeIds).toBe("function");
+    expect(typeof encoding.getWords).toBe("function");
+    expect(typeof encoding.pad).toBe("function");
+    expect(typeof encoding.truncate).toBe("function");
+  });
+
+  describe("truncate", () => {
+    it("accepts `undefined` as second parameter", () => {
+      expect(encoding.truncate(10, undefined)).toBeUndefined();
+    });
+  });
+
+  describe("getWords", () => {
+    it("returns the correct list of indexes", () => {
+      const indexes = encoding.getWords();
+      expect(indexes).toEqual([0, 1, 2, 3, 3]);
+    });
+  });
+
+  describe("wordToTokens", () => {
+    it("returns the correct indexes", () => {
+      const indexes = encoding.wordToTokens(3);
+      expect(indexes).toEqual([3, 5]);
     });
 
-    beforeEach(async () => {
-        encoding = await encode(originalString, null);
+    it("returns undefined when out of range word", () => {
+      const index = encoding.wordToTokens(100);
+      expect(index).toBeUndefined();
+    });
+  });
+
+  describe("wordToChars", () => {
+    it("returns the correct offsets", () => {
+      const offsets = encoding.wordToChars(3);
+      expect(offsets).toEqual([11, 15]);
     });
 
-    it("has a list of defined methods", async () => {
-        expect(typeof encoding.wordToTokens).toBe("function");
-        expect(typeof encoding.wordToChars).toBe("function");
-        expect(typeof encoding.tokenToChars).toBe("function");
-        expect(typeof encoding.tokenToWord).toBe("function");
-        expect(typeof encoding.charToToken).toBe("function");
-        expect(typeof encoding.charToWord).toBe("function");
-        expect(typeof encoding.getAttentionMask).toBe("function");
-        expect(typeof encoding.getIds).toBe("function");
-        expect(typeof encoding.getLength).toBe("function");
-        expect(typeof encoding.getOffsets).toBe("function");
-        expect(typeof encoding.getOverflowing).toBe("function");
-        expect(typeof encoding.getSpecialTokensMask).toBe("function");
-        expect(typeof encoding.getTokens).toBe("function");
-        expect(typeof encoding.getTypeIds).toBe("function");
-        expect(typeof encoding.getWords).toBe("function");
-        expect(typeof encoding.pad).toBe("function");
-        expect(typeof encoding.truncate).toBe("function");
+    it("returns undefined when out of range word", () => {
+      const offsets = encoding.wordToChars(100);
+      expect(offsets).toBeUndefined();
+    });
+  });
+
+  describe("tokenToChars", () => {
+    it("returns the correct offsets", () => {
+      const offsets = encoding.tokenToChars(3);
+      expect(offsets).toEqual([11, 13]);
     });
 
-    describe("truncate", () => {
-        it("accepts `undefined` as second parameter", () => {
-            expect(encoding.truncate(10, undefined)).toBeUndefined();
-        });
+    it("returns undefined when out of range token", () => {
+      const offsets = encoding.tokenToChars(100);
+      expect(offsets).toBeUndefined();
+    });
+  });
+
+  describe("tokenToWord", () => {
+    it("returns the correct index", () => {
+      const index = encoding.tokenToWord(3);
+      expect(index).toEqual(3);
     });
 
-    describe("getWords", () => {
-        it("returns the correct list of indexes", () => {
-            const indexes = encoding.getWords();
-            expect(indexes).toEqual([0, 1, 2, 3, 3]);
-        });
+    it("returns undefined when out of range token", () => {
+      const index = encoding.tokenToWord(100);
+      expect(index).toBeUndefined();
+    });
+  });
+
+  describe("charToToken", () => {
+    it("returns the correct index", () => {
+      const index = encoding.charToToken(3);
+      expect(index).toEqual(1);
     });
 
-    describe("wordToTokens", () => {
-        it("returns the correct indexes", () => {
-            const indexes = encoding.wordToTokens(3);
-            expect(indexes).toEqual([3, 5]);
-        });
+    it("returns undefined when out of range char", () => {
+      const index = encoding.charToToken(100);
+      expect(index).toBeUndefined();
+    });
+  });
 
-        it("returns undefined when out of range word", () => {
-            const index = encoding.wordToTokens(100);
-            expect(index).toBeUndefined();
-        });
+  describe("charToWord", () => {
+    it("returns the correct index", () => {
+      const index = encoding.charToWord(3);
+      expect(index).toEqual(1);
     });
 
-    describe("wordToChars", () => {
-        it("returns the correct offsets", () => {
-            const offsets = encoding.wordToChars(3);
-            expect(offsets).toEqual([11, 15]);
-        });
+    it("returns undefined when out of range char", () => {
+      const index = encoding.charToWord(100);
+      expect(index).toBeUndefined();
+    });
+  });
 
-        it("returns undefined when out of range word", () => {
-            const offsets = encoding.wordToChars(100);
-            expect(offsets).toBeUndefined();
-        });
+  describe("pad", () => {
+    it("works correctly with only one parameter", () => {
+      encoding.pad(10);
+      expect(encoding.getTokens()).toHaveLength(10);
     });
 
-    describe("tokenToChars", () => {
-        it("returns the correct offsets", () => {
-            const offsets = encoding.tokenToChars(3);
-            expect(offsets).toEqual([11, 13]);
-        });
-
-        it("returns undefined when out of range token", () => {
-            const offsets = encoding.tokenToChars(100);
-            expect(offsets).toBeUndefined();
-        });
+    it("accepts `undefined` as second parameter", () => {
+      encoding.pad(10, undefined);
+      expect(encoding.getTokens()).toHaveLength(10);
     });
 
-    describe("tokenToWord", () => {
-        it("returns the correct index", () => {
-            const index = encoding.tokenToWord(3);
-            expect(index).toEqual(3);
-        });
+    it("accepts options as second parameter", () => {
+      encoding.pad(10, {
+        direction: PaddingDirection.Left,
+        padToken: "[PA]",
+        padTypeId: 10,
+        padId: 400,
+      });
 
-        it("returns undefined when out of range token", () => {
-            const index = encoding.tokenToWord(100);
-            expect(index).toBeUndefined();
-        });
+      const tokens = encoding.getTokens();
+      expect(tokens).toHaveLength(10);
+      expect(tokens[0]).toBe("[PA]");
+      expect(encoding.getTypeIds()[0]).toBe(10);
+      expect(encoding.getIds()[0]).toBe(400);
     });
-
-    describe("charToToken", () => {
-        it("returns the correct index", () => {
-            const index = encoding.charToToken(3);
-            expect(index).toEqual(1);
-        });
-
-        it("returns undefined when out of range char", () => {
-            const index = encoding.charToToken(100);
-            expect(index).toBeUndefined();
-        });
-    });
-
-    describe("charToWord", () => {
-        it("returns the correct index", () => {
-            const index = encoding.charToWord(3);
-            expect(index).toEqual(1);
-        });
-
-        it("returns undefined when out of range char", () => {
-            const index = encoding.charToWord(100);
-            expect(index).toBeUndefined();
-        });
-    });
-
-    describe("pad", () => {
-        it("works correctly with only one parameter", () => {
-            encoding.pad(10);
-            expect(encoding.getTokens()).toHaveLength(10);
-        });
-
-        it("accepts `undefined` as second parameter", () => {
-            encoding.pad(10, undefined);
-            expect(encoding.getTokens()).toHaveLength(10);
-        });
-
-        it("accepts options as second parameter", () => {
-            encoding.pad(10, {
-                direction: PaddingDirection.Left,
-                padToken: "[PA]",
-                padTypeId: 10,
-                padId: 400
-            });
-
-            const tokens = encoding.getTokens();
-            expect(tokens).toHaveLength(10);
-            expect(tokens[0]).toBe("[PA]");
-            expect(encoding.getTypeIds()[0]).toBe(10);
-            expect(encoding.getIds()[0]).toBe(400);
-        });
-    });
+  });
 });
