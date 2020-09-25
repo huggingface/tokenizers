28dbc356c27689788e8003e605491c156d2d8773
Nicolas Patry <patry.nicolas@protonmail.com>
2020-09-25 21:43:02 +0200

    Attempt to get some documentation going.

--- bindings/node/lib/index.ts
+++ bindings/node/lib/index.ts
@@ -10,7 +10,14 @@ export {
   InputSequence,
   EncodeInput,
   EncodeOptions,
+  Tokenizer,
   TruncationConfiguration,
   TruncationOptions,
 } from "./bindings/tokenizer";
+export * as models from "./bindings/models";
+export * as normalizers from "./bindings/normalizers";
+export * as pre_tokenizers from "./bindings/pre-tokenizers";
+export * as decoders from "./bindings/decoders";
+export * as post_processors from "./bindings/post-processors";
+export * as trainers from "./bindings/trainers";
 export { Encoding } from "./implementations/encoding";
