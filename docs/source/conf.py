6b1b7551f40eec4cf0418aea4e3e91f5488ee8c1
Anthony MOI <m.anthony.moi@gmail.com>
2020-10-01 17:22:02 -0400

    Doc - Use huggingface theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,3 +54,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+
+def setup(app):
+    app.add_css_file("css/huggingface.css")
+    app.add_css_file("css/code-snippets.css")
