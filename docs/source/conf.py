37e68fbcf90259aeb2d210e167f53980a99b705f
Anthony MOI <m.anthony.moi@gmail.com>
2020-10-05 11:00:25 -0400

    Doc - Quick js/css update + remove sphinx_tabs deps

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,9 +30,7 @@ release = "0.9.0"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx_tabs.tabs",
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
