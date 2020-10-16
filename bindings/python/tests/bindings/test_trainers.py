71bdd1d6104f930cd9e741278226a77e4e2eae65
Nicolas Patry <patry.nicolas@protonmail.com>
2020-10-16 16:51:15 +0200

    Fixing hanging error while acquiring GIL from custom pretokenizer during training.
    Fixes #469

--- bindings/python/tests/bindings/test_trainers.py
+++ bindings/python/tests/bindings/test_trainers.py
@@ -2,7 +2,14 @@ import os
 import pytest
 import pickle
 
-from tokenizers import SentencePieceUnigramTokenizer
+from tokenizers import (
+    SentencePieceUnigramTokenizer,
+    models,
+    pre_tokenizers,
+    normalizers,
+    Tokenizer,
+    trainers,
+)
 from ..utils import data_dir, train_files
 
 
@@ -14,3 +21,28 @@ class TestUnigram:
         filename = "tests/data/unigram_trained.json"
         tokenizer.save(filename)
         os.remove(filename)
+
+    def test_train_parallelism_with_custom_pretokenizer(self, train_files):
+        tokenizer = SentencePieceUnigramTokenizer()
+
+        class GoodCustomPretok:
+            def split(self, n, normalized):
+                #  Here we just test that we can return a List[NormalizedString], it
+                # does not really make sense to return twice the same otherwise
+                return [normalized, normalized]
+
+            def pre_tokenize(self, pretok):
+                pretok.split(self.split)
+
+        custom = pre_tokenizers.PreTokenizer.custom(GoodCustomPretok())
+        bpe_tokenizer = Tokenizer(models.BPE())
+        bpe_tokenizer.normalizer = normalizers.Lowercase()
+        bpe_tokenizer.pre_tokenizer = custom
+
+        tokenizer.pre_tokenizer = custom
+
+        if "TOKENIZERS_PARALLELISM" in os.environ:
+            del os.environ["TOKENIZERS_PARALLELISM"]
+
+        trainer = trainers.BpeTrainer(special_tokens=["<unk>"], show_progress=False)
+        bpe_tokenizer.train(trainer, [train_files["small"]])
