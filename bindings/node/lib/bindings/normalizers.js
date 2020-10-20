cfc986e28480a7d2f56bace23c1ebcbe0978f19d
ecchochan <ecchochan@gmail.com>
2020-10-20 16:08:35 +0800

    Formatting :S

--- bindings/node/lib/bindings/normalizers.js
+++ bindings/node/lib/bindings/normalizers.js
@@ -1,11 +1,11 @@
 const native = require("./native");
 
 const normOptions = {
-    SEPARATE_INTEGERS: 1 << 1,
-    SEPARATE_SYMBOLS: 1 << 2,
-    SIMPL_TO_TRAD: 1 << 3,
-    TRAD_TO_SIMPL: 1 << 4,
-    ZH_NORM_MAPPING: 1 << 5
+  SEPARATE_INTEGERS: 1 << 1,
+  SEPARATE_SYMBOLS: 1 << 2,
+  SIMPL_TO_TRAD: 1 << 3,
+  TRAD_TO_SIMPL: 1 << 4,
+  ZH_NORM_MAPPING: 1 << 5,
 };
 
 module.exports = {
@@ -18,6 +18,10 @@ module.exports = {
   sequenceNormalizer: native.normalizers_Sequence,
   stripNormalizer: native.normalizers_Strip,
   stripAccentsNormalizer: native.normalizers_StripAccents,
-  openccEnabled: native.normalizers_OpenccEnabled || function(){return false},
-  normOptions: normOptions
+  openccEnabled:
+    native.normalizers_OpenccEnabled ||
+    function () {
+      return false;
+    },
+  normOptions: normOptions,
 };
