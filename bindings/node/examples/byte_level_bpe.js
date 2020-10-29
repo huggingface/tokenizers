b6ffd9cba000e1a5fd97af4065db6f53cc9b251a
Anthony MOI <m.anthony.moi@gmail.com>
2020-10-29 12:29:46 -0400

    Doc - Cleanup old tests & node lints

--- bindings/node/examples/byte_level_bpe.js
+++
@@ -1,8 +0,0 @@
-const tokenizers = require("..");
-
-let bpe = tokenizers.models.BPE.fromFiles(
-  "./data/gpt2-vocab.json",
-  "./data/gpt2-merges.txt"
-);
-let tokenizer = new tokenizers.Tokenizer(bpe);
-console.log(bpe, tokenizer);
--- bindings/node/examples/documentation/quicktour.test.ts
+++ bindings/node/examples/documentation/quicktour.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 var globRequire = require;
 
 describe("quicktourExample", () => {
