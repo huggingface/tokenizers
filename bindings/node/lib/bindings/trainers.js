ae88a55ed97224750f4434f6bc6370339ecf5033
Anthony MOI <m.anthony.moi@gmail.com>
2020-10-30 11:18:54 -0400

    Node - Add UnigramTrainer

--- bindings/node/lib/bindings/trainers.js
+++ bindings/node/lib/bindings/trainers.js
@@ -3,4 +3,5 @@ const native = require("./native");
 module.exports = {
   bpeTrainer: native.trainers_BPETrainer,
   wordPieceTrainer: native.trainers_WordPieceTrainer,
+  unigramTrainer: native.trainers_UnigramTrainer,
 };
