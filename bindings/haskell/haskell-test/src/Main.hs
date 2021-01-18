414065686d46f9787b875ec3f423c8cf70a43a18
austinvhuang <austinh@alum.mit.edu>
2021-01-17 23:19:05 -0500

    test getTokens array of strings return value

--- bindings/haskell/haskell-test/src/Main.hs
+++ bindings/haskell/haskell-test/src/Main.hs
@@ -6,6 +6,10 @@ test string tokenizer = do
   putStrLn $ "\n----\n" ++ string ++ ""
   tokenize string tokenizer
 
+  putStrLn "\nRetrieve data in Haskell\n"
+  encoding <- encode tokenizer string
+  getTokens encoding
+
 main :: IO ()
 main = do
   tokenizer <- mkRobertaTokenizer "roberta-base-vocab.json" "roberta-base-merges.txt"
@@ -16,3 +20,4 @@ main = do
   test "It was the best of times it was the blurst of times." tokenizer
   test "It is precisely the common features of all experience, such as characterise everything we encounter, which are the primary and most profound occasion for astonishment; indeed, one might almost say that it is the fact that anything is experienced and encountered at all." tokenizer
   test "What does 'organic' mean?-that is, in the wider sense here supposed, naturally excluding such simple answers as 'protei' or 'protoplasm'. Fixing our attention on a somewhat wider concept than this, we arrive at the criterion of metabolism. Thus Schopenhauer's line of demarcation may be regarded as highly suitable, when he says that in inorganic being 'the essential and permanent element, the basis of identity and integrity, is the material, the matter, the inessential and mutable element being the form. In organic being the reverse is true; for its life, that is, its existence as an organic being, consists precisely in a constant change of matter while the form persists.'" tokenizer
+
