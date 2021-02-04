a8f756494ebaa04a5c44c42249e569ff5855c6ad
Anthony MOI <m.anthony.moi@gmail.com>
2021-02-04 09:59:18 -0500

    Improve Model serialization/deserialization (#620)

--- tokenizers/src/models/wordlevel/serialization.rs
+++ tokenizers/src/models/wordlevel/serialization.rs
@@ -4,14 +4,16 @@ use serde::{
     ser::SerializeStruct,
     Deserialize, Deserializer, Serialize, Serializer,
 };
+use std::collections::HashSet;
 
 impl Serialize for WordLevel {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let mut model = serializer.serialize_struct("WordLevel", 2)?;
+        let mut model = serializer.serialize_struct("WordLevel", 3)?;
         let ordered_vocab = OrderedVocabIter::new(&self.vocab_r);
+        model.serialize_field("type", "WordLevel")?;
         model.serialize_field("vocab", &ordered_vocab)?;
         model.serialize_field("unk_token", &self.unk_token)?;
         model.end()
@@ -23,7 +25,11 @@ impl<'de> Deserialize<'de> for WordLevel {
     where
         D: Deserializer<'de>,
     {
-        deserializer.deserialize_struct("WordLevel", &["vocab", "unk_token"], WordLevelVisitor)
+        deserializer.deserialize_struct(
+            "WordLevel",
+            &["type", "vocab", "unk_token"],
+            WordLevelVisitor,
+        )
     }
 }
 
@@ -40,13 +46,66 @@ impl<'de> Visitor<'de> for WordLevelVisitor {
         V: MapAccess<'de>,
     {
         let mut builder = WordLevelBuilder::new();
+        let mut missing_fields = vec![
+            // for retrocompatibility the "type" field is not mandatory
+            "unk_token",
+            "vocab",
+        ]
+        .into_iter()
+        .collect::<HashSet<_>>();
         while let Some(key) = map.next_key::<String>()? {
             match key.as_ref() {
                 "vocab" => builder = builder.vocab(map.next_value()?),
                 "unk_token" => builder = builder.unk_token(map.next_value()?),
+                "type" => match map.next_value()? {
+                    "WordLevel" => {}
+                    u => {
+                        return Err(serde::de::Error::invalid_value(
+                            serde::de::Unexpected::Str(u),
+                            &"WordLevel",
+                        ))
+                    }
+                },
                 _ => {}
             }
+            missing_fields.remove::<str>(&key);
         }
-        Ok(builder.build().map_err(serde::de::Error::custom)?)
+
+        if !missing_fields.is_empty() {
+            Err(serde::de::Error::missing_field(
+                missing_fields.iter().next().unwrap(),
+            ))
+        } else {
+            Ok(builder.build().map_err(serde::de::Error::custom)?)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn serde() {
+        let wl = WordLevel::default();
+        let wl_s = r#"{"type":"WordLevel","vocab":{},"unk_token":"<unk>"}"#;
+
+        assert_eq!(serde_json::to_string(&wl).unwrap(), wl_s);
+        assert_eq!(serde_json::from_str::<WordLevel>(wl_s).unwrap(), wl);
+    }
+
+    #[test]
+    fn deserialization_should_fail() {
+        let missing_unk = r#"{"type":"WordLevel","vocab":{}}"#;
+        assert!(serde_json::from_str::<WordLevel>(missing_unk)
+            .unwrap_err()
+            .to_string()
+            .starts_with("missing field `unk_token`"));
+
+        let wrong_type = r#"{"type":"WordPiece","vocab":{}}"#;
+        assert!(serde_json::from_str::<WordLevel>(wrong_type)
+            .unwrap_err()
+            .to_string()
+            .starts_with("invalid value: string \"WordPiece\", expected WordLevel"));
     }
 }
--- tokenizers/src/models/wordpiece/serialization.rs
+++ tokenizers/src/models/wordpiece/serialization.rs
@@ -4,15 +4,17 @@ use serde::{
     ser::SerializeStruct,
     Deserialize, Deserializer, Serialize, Serializer,
 };
+use std::collections::HashSet;
 
 impl Serialize for WordPiece {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let mut model = serializer.serialize_struct("WordPiece", 4)?;
+        let mut model = serializer.serialize_struct("WordPiece", 5)?;
 
         // Small fields first
+        model.serialize_field("type", "WordPiece")?;
         model.serialize_field("unk_token", &self.unk_token)?;
         model.serialize_field("continuing_subword_prefix", &self.continuing_subword_prefix)?;
         model.serialize_field("max_input_chars_per_word", &self.max_input_chars_per_word)?;
@@ -33,6 +35,7 @@ impl<'de> Deserialize<'de> for WordPiece {
         deserializer.deserialize_struct(
             "WordPiece",
             &[
+                "type",
                 "unk_token",
                 "continuing_subword_prefix",
                 "max_input_chars_per_word",
@@ -56,6 +59,16 @@ impl<'de> Visitor<'de> for WordPieceVisitor {
         V: MapAccess<'de>,
     {
         let mut builder = WordPieceBuilder::new();
+        let mut missing_fields = vec![
+            // for retrocompatibility the "type" field is not mandatory
+            "unk_token",
+            "continuing_subword_prefix",
+            "max_input_chars_per_word",
+            "vocab",
+        ]
+        .into_iter()
+        .collect::<HashSet<_>>();
+
         while let Some(key) = map.next_key::<String>()? {
             match key.as_ref() {
                 "unk_token" => builder = builder.unk_token(map.next_value()?),
@@ -66,9 +79,70 @@ impl<'de> Visitor<'de> for WordPieceVisitor {
                     builder = builder.max_input_chars_per_word(map.next_value()?)
                 }
                 "vocab" => builder = builder.vocab(map.next_value()?),
+                "type" => match map.next_value()? {
+                    "WordPiece" => {}
+                    u => {
+                        return Err(serde::de::Error::invalid_value(
+                            serde::de::Unexpected::Str(u),
+                            &"WordPiece",
+                        ))
+                    }
+                },
                 _ => {}
             }
+            missing_fields.remove::<str>(&key);
         }
-        Ok(builder.build().map_err(serde::de::Error::custom)?)
+
+        if !missing_fields.is_empty() {
+            Err(serde::de::Error::missing_field(
+                missing_fields.iter().next().unwrap(),
+            ))
+        } else {
+            Ok(builder.build().map_err(serde::de::Error::custom)?)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn serde() {
+        let wp = WordPiece::default();
+        let wp_s = "{\
+            \"type\":\"WordPiece\",\
+            \"unk_token\":\"[UNK]\",\
+            \"continuing_subword_prefix\":\"##\",\
+            \"max_input_chars_per_word\":100,\
+            \"vocab\":{}\
+        }";
+
+        assert_eq!(serde_json::to_string(&wp).unwrap(), wp_s);
+        assert_eq!(serde_json::from_str::<WordPiece>(wp_s).unwrap(), wp);
+    }
+
+    #[test]
+    fn deserialization_should_fail() {
+        let missing_unk = "{\
+            \"type\":\"WordPiece\",\
+            \"continuing_subword_prefix\":\"##\",\
+            \"max_input_chars_per_word\":100,\
+            \"vocab\":{}\
+        }";
+        assert!(serde_json::from_str::<WordPiece>(missing_unk)
+            .unwrap_err()
+            .to_string()
+            .starts_with("missing field `unk_token`"));
+
+        let wrong_type = "{\
+            \"type\":\"WordLevel\",\
+            \"unk_token\":\"[UNK]\",\
+            \"vocab\":{}\
+        }";
+        assert!(serde_json::from_str::<WordPiece>(wrong_type)
+            .unwrap_err()
+            .to_string()
+            .starts_with("invalid value: string \"WordLevel\", expected WordPiece"));
     }
 }
