70826cb70fe636d61a8cb3728f1ceddfddd8944d
austinvhuang <austinh@alum.mit.edu>
2021-04-10 13:10:06 -0400

    package as exported library, fix up makefiles, wip - getIDs dont look valid

--- bindings/haskell/tokenizers-haskell/src/Main.hs
+++ bindings/haskell/tokenizers-haskell/src/Main.hs
@@ -23,5 +23,7 @@ main = do
   test "It is precisely the common features of all experience, such as characterise everything we encounter, which are the primary and most profound occasion for astonishment; indeed, one might almost say that it is the fact that anything is experienced and encountered at all." tokenizer
   test "What does 'organic' mean?-that is, in the wider sense here supposed, naturally excluding such simple answers as 'protei' or 'protoplasm'. Fixing our attention on a somewhat wider concept than this, we arrive at the criterion of metabolism. Thus Schopenhauer's line of demarcation may be regarded as highly suitable, when he says that in inorganic being 'the essential and permanent element, the basis of identity and integrity, is the material, the matter, the inessential and mutable element being the form. In organic being the reverse is true; for its life, that is, its existence as an organic being, consists precisely in a constant change of matter while the form persists.'" tokenizer
   test "hey hey there hey hey hey there hey" tokenizer
-  test "hi hi hi" tokenizer
+  test "hi hi hi hello hello" tokenizer
+  test "hi" tokenizer
+  test "hi" tokenizer
 
