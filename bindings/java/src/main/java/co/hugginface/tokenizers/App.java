4423536fc9f063448ff19db42b9aaba64c6acc9b
andreaduque <ad@hypefactors.com>
2021-11-22 16:45:45 +0100

    experiment with JNA pointers

--- bindings/java/src/main/java/co/hugginface/tokenizers/App.java
+++ bindings/java/src/main/java/co/hugginface/tokenizers/App.java
@@ -1,14 +1,31 @@
 package co.hugginface.tokenizers;
 
+import com.sun.jna.*;
+
+class CBertTokenizer extends PointerType {
+    private JnaCBertTokenizer INTERFACE = JnaCBertTokenizer.INSTANCE;
+
+    public CBertTokenizer() {
+        Pointer pointer = INTERFACE.CBertTokenizer_new();
+        this.setPointer(pointer);
+    }
+    public void close() {
+        Pointer p = this.getPointer();
+        INTERFACE.CBertTokenizer_drop(p);
+    }
+    public void someMethod(){
+        Pointer p = this.getPointer();
+        INTERFACE.CBertTokenizer_some_method(p);
+    }
+
+}
+
 public class App {
 
 
     public static void main(String[] args) {
-
-        RustJNAInterface INTERFACE = RustJNAInterface.INSTANCE;
-
-        INTERFACE.rust_function();
-
-
+        CBertTokenizer tokenizer = new CBertTokenizer();
+        tokenizer.someMethod();
+        tokenizer.close();
     }
 }
