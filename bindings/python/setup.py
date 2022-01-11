8240bfa6446fa078aa7e0b9c6c05c02ef75d4dd3
jc-louis <jean-charles.louis@lifen.fr>
2022-01-11 13:49:46 +0100

    add missing python3.9 classifier

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -30,6 +30,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     package_dir={"": "py_src"},
