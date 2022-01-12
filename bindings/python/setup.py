cabbecb96c940b9edb27847f3b0699fbcc06d058
JC Louis <jean-charles.louis@lifen.fr>
2022-01-12 09:42:13 +0100

    add python3.10 release (#877)
    * add missing python3.9 classifier
    
    * add python3.10 release
    
    * run tests on 3.10
    
    * Revert "run tests on 3.10"
    
    This reverts commit ceed64249e54b6ec622b06c59bf47da7c6dfc1b0.

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -30,6 +30,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     package_dir={"": "py_src"},
