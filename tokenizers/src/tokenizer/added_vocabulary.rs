6e8b701870d9e89696db3ee9ebfa592dcb378b71
Nicolas Patry <patry.nicolas@protonmail.com>
2022-02-24 15:50:26 +0100

    Adding warning when unexpected code path is taken.

--- tokenizers/src/tokenizer/added_vocabulary.rs
+++ tokenizers/src/tokenizer/added_vocabulary.rs
@@ -111,6 +111,9 @@ fn space_leftmost_at_end(sentence: &str) -> usize {
         match_.start()
     } else {
         // This should never happen since the Regex should match all the time
+        // It didn't match any space, so we can safely return sentence.len() to be the leftmot space and the
+        // end
+        warn!("Warning in leftmost_at_end code path, we expect the regex to match always and it didn't, Please report this.");
         sentence.len()
     }
 }
@@ -119,6 +122,9 @@ fn space_rightmost_at_start(sentence: &str) -> usize {
         match_.end()
     } else {
         // This should never happen since the Regex should match all the time
+        // It didn't match any space, so we can safely return 0 to be the rightmost space and the
+        // start
+        warn!("Warning in leftmost_at_end code path, we expect the regex to match always and it didn't, Please report this.");
         0
     }
 }
