33fa2b203c62dd0fcefbf299e8bd2d3767ba477c
Luc Georges <McPatate@users.noreply.github.com>
2022-03-02 12:24:51 +0100

    feat(ci): for loop in bdist_wheel script for m1
    Co-authored-by: Nicolas Patry <patry.nicolas@protonmail.com>

--- bindings/python/tokenizers_manual_m1_build.sh
+++ bindings/python/tokenizers_manual_m1_build.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
+for VARIABLE in "3.7.12" "3.8.12" "3.9.10" "3.10.2"
+do
+    MACOSX_DEPLOYMENT_TARGET=10.11 SDKROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk" CFLAGS="-I/usr/include/openssl -I/usr/local/opt/readline/include -I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include" CPPFLAGS="-I/usr/local/opt/zlib/include" LDFLAGS="-L/usr/lib -L/usr/local/opt/readline/lib" pyenv install $VARIABLE
 
-MACOSX_DEPLOYMENT_TARGET=10.11 SDKROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk" CFLAGS="-I/usr/include/openssl -I/usr/local/opt/readline/include -I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include" CPPFLAGS="-I/usr/local/opt/zlib/include" LDFLAGS="-L/usr/lib -L/usr/local/opt/readline/lib" pyenv install <wtv_version>
+    ~/.pyenv/versions/$VARIABLE/bin/pip install setuptools wheel setuptools-rust==0.11.3 --ignore-installed --force-reinstall
 
-~/.pyenv/versions/<wtv_version>/bin/pip install setuptools wheel setuptools-rust==0.11.3 --ignore-installed --force-reinstall
-
-MACOSX_DEPLOYMENT_TARGET=10.11 ~/.pyenv/versions/<wtv_version>/bin/python setup.py bdist_wheel
-
+    MACOSX_DEPLOYMENT_TARGET=10.11 ~/.pyenv/versions/$VARIABLE/bin/python setup.py bdist_wheel
+done
