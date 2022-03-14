a21ae03ba3ff05658fd0bd2c7381245006babe39
Nicolas Patry <patry.nicolas@protonmail.com>
2022-03-14 11:13:23 +0100

    Making sure serialized format is written in python tests.

--- bindings/python/tests/bindings/test_trainers.py
+++ bindings/python/tests/bindings/test_trainers.py
@@ -59,6 +59,10 @@ class TestBpeTrainer:
         assert trainer.continuing_subword_prefix == None
 
     def test_can_pickle(self):
+        assert (
+            trainers.BpeTrainer(min_frequency=12).__getstate__()
+            == b"""{"BpeTrainer":{"min_frequency":12,"vocab_size":30000,"show_progress":true,"special_tokens":[],"limit_alphabet":null,"initial_alphabet":[],"continuing_subword_prefix":null,"end_of_word_suffix":null,"words":{}}}"""
+        )
         assert isinstance(
             pickle.loads(pickle.dumps(trainers.BpeTrainer(min_frequency=12))), trainers.BpeTrainer
         )
