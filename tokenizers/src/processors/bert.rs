a1ffa36b2deef42dcf730e408fc7642de3dc51ff
Nicolas Patry <patry.nicolas@protonmail.com>
2022-08-24 09:38:25 +0200

    Making `process_encodings` not eat up the encodings any more.

--- tokenizers/src/processors/bert.rs
+++ tokenizers/src/processors/bert.rs
@@ -1,4 +1,4 @@
-use crate::tokenizer::{Encoding, PostProcessor, ProcessorError, Result};
+use crate::tokenizer::{Encoding, PostProcessor, Result};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::iter::FromIterator;
@@ -49,53 +49,11 @@ impl PostProcessor for BertProcessing {
             return Ok(encodings);
         }
 
-        let (mut encoding, pair_encoding): (Encoding, Option<Encoding>) = match encodings.len() {
-            1 => (
-                encodings
-                    .pop()
-                    .ok_or(ProcessorError::InvalidEncodingsVecLength)?,
-                None,
-            ),
-            2 => {
-                let pair = encodings
-                    .pop()
-                    .ok_or(ProcessorError::InvalidEncodingsVecLength)?;
-                let encoding = encodings
-                    .pop()
-                    .ok_or(ProcessorError::InvalidEncodingsVecLength)?;
-                (encoding, Some(pair))
-            }
-            _ => return Err(Box::new(ProcessorError::InvalidEncodingsVecLength)),
-        };
-
-        let ids = [&[self.cls.1], encoding.get_ids(), &[self.sep.1]].concat();
-        let type_ids = [&[0], encoding.get_type_ids(), &[0]].concat();
-        let tokens = [
-            &[self.cls.0.clone()],
-            encoding.get_tokens(),
-            &[self.sep.0.clone()],
-        ]
-        .concat();
-        let words = [&[None], encoding.get_word_ids(), &[None]].concat();
-        let offsets = [&[(0, 0)], encoding.get_offsets(), &[(0, 0)]].concat();
-        let special_tokens = [&[1u32], &vec![0; encoding.get_ids().len()][..], &[1]].concat();
-        let attention_mask = vec![1; ids.len()];
-
-        // For compatibility with `TemplateProcessing`, the sequence_ranges shouldn't contain
-        // the special tokens.
-        let sequence_ranges = HashMap::from_iter(vec![(0, 1..ids.len() - 1)]);
-        let mut new_encoding = Encoding::new(
-            ids,
-            type_ids,
-            tokens,
-            words,
-            offsets,
-            special_tokens,
-            attention_mask,
-            encoding
-                .take_overflowing()
-                .into_iter()
-                .map(|encoding| {
+        let encodings: Vec<Encoding> = encodings
+            .iter_mut()
+            .enumerate()
+            .map(|(i, encoding)| {
+                if i == 0 {
                     let ids = [&[self.cls.1], encoding.get_ids(), &[self.sep.1]].concat();
                     let type_ids = [&[0], encoding.get_type_ids(), &[0]].concat();
                     let tokens = [
@@ -110,8 +68,8 @@ impl PostProcessor for BertProcessing {
                         [&[1u32], &vec![0; encoding.get_ids().len()][..], &[1]].concat();
                     let attention_mask = vec![1; ids.len()];
 
-                    // For compatibility with `TemplateProcessing`, the sequence_ranges shouldn't
-                    // contain the special tokens.
+                    // For compatibility with `TemplateProcessing`, the sequence_ranges shouldn't contain
+                    // the special tokens.
                     let sequence_ranges = HashMap::from_iter(vec![(0, 1..ids.len() - 1)]);
                     Encoding::new(
                         ids,
@@ -121,72 +79,105 @@ impl PostProcessor for BertProcessing {
                         offsets,
                         special_tokens,
                         attention_mask,
-                        vec![],
+                        encoding
+                            .take_overflowing()
+                            .into_iter()
+                            .map(|encoding| {
+                                let ids =
+                                    [&[self.cls.1], encoding.get_ids(), &[self.sep.1]].concat();
+                                let type_ids = [&[0], encoding.get_type_ids(), &[0]].concat();
+                                let tokens = [
+                                    &[self.cls.0.clone()],
+                                    encoding.get_tokens(),
+                                    &[self.sep.0.clone()],
+                                ]
+                                .concat();
+                                let words = [&[None], encoding.get_word_ids(), &[None]].concat();
+                                let offsets =
+                                    [&[(0, 0)], encoding.get_offsets(), &[(0, 0)]].concat();
+                                let special_tokens =
+                                    [&[1u32], &vec![0; encoding.get_ids().len()][..], &[1]]
+                                        .concat();
+                                let attention_mask = vec![1; ids.len()];
+
+                                // For compatibility with `TemplateProcessing`, the sequence_ranges shouldn't
+                                // contain the special tokens.
+                                let sequence_ranges =
+                                    HashMap::from_iter(vec![(0, 1..ids.len() - 1)]);
+                                Encoding::new(
+                                    ids,
+                                    type_ids,
+                                    tokens,
+                                    words,
+                                    offsets,
+                                    special_tokens,
+                                    attention_mask,
+                                    vec![],
+                                    sequence_ranges,
+                                )
+                            })
+                            .collect(),
                         sequence_ranges,
                     )
-                })
-                .collect(),
-            sequence_ranges,
-        );
+                } else {
+                    let pair_ids = [encoding.get_ids(), &[self.sep.1]].concat();
+                    let pair_type_ids = [encoding.get_type_ids(), &[1]].concat();
+                    let pair_tokens = [encoding.get_tokens(), &[self.sep.0.clone()]].concat();
+                    let pair_words = [encoding.get_word_ids(), &[None]].concat();
+                    let pair_offsets = [encoding.get_offsets(), &[(0, 0)]].concat();
+                    let pair_special_tokens =
+                        [&vec![0u32; encoding.get_type_ids().len()][..], &[1]].concat();
+                    let pair_attention_mask = vec![1; pair_ids.len()];
 
-        if let Some(mut encoding) = pair_encoding {
-            let pair_ids = [encoding.get_ids(), &[self.sep.1]].concat();
-            let pair_type_ids = [encoding.get_type_ids(), &[1]].concat();
-            let pair_tokens = [encoding.get_tokens(), &[self.sep.0.clone()]].concat();
-            let pair_words = [encoding.get_word_ids(), &[None]].concat();
-            let pair_offsets = [encoding.get_offsets(), &[(0, 0)]].concat();
-            let pair_special_tokens =
-                [&vec![0u32; encoding.get_type_ids().len()][..], &[1]].concat();
-            let pair_attention_mask = vec![1; pair_ids.len()];
+                    // For compatibility with `TemplateProcessing`, the sequence_ranges shouldn't contain
+                    // the special tokens.
+                    let pair_sequence_ranges = HashMap::from_iter(vec![(1, 0..pair_ids.len() - 1)]);
+                    Encoding::new(
+                        pair_ids,
+                        pair_type_ids,
+                        pair_tokens,
+                        pair_words,
+                        pair_offsets,
+                        pair_special_tokens,
+                        pair_attention_mask,
+                        encoding
+                            .take_overflowing()
+                            .into_iter()
+                            .map(|encoding| {
+                                let pair_ids = [encoding.get_ids(), &[self.sep.1]].concat();
+                                let pair_type_ids = [encoding.get_type_ids(), &[1]].concat();
+                                let pair_tokens =
+                                    [encoding.get_tokens(), &[self.sep.0.clone()]].concat();
+                                let pair_words = [encoding.get_word_ids(), &[None]].concat();
+                                let pair_offsets = [encoding.get_offsets(), &[(0, 0)]].concat();
+                                let pair_special_tokens =
+                                    [&vec![0u32; encoding.get_type_ids().len()][..], &[1]].concat();
+                                let pair_attention_mask = vec![1; pair_ids.len()];
 
-            // For compatibility with `TemplateProcessing`, the sequence_ranges shouldn't contain
-            // the special tokens.
-            let pair_sequence_ranges = HashMap::from_iter(vec![(1, 0..pair_ids.len() - 1)]);
-            let new_pair_encoding = Encoding::new(
-                pair_ids,
-                pair_type_ids,
-                pair_tokens,
-                pair_words,
-                pair_offsets,
-                pair_special_tokens,
-                pair_attention_mask,
-                encoding
-                    .take_overflowing()
-                    .into_iter()
-                    .map(|encoding| {
-                        let pair_ids = [encoding.get_ids(), &[self.sep.1]].concat();
-                        let pair_type_ids = [encoding.get_type_ids(), &[1]].concat();
-                        let pair_tokens = [encoding.get_tokens(), &[self.sep.0.clone()]].concat();
-                        let pair_words = [encoding.get_word_ids(), &[None]].concat();
-                        let pair_offsets = [encoding.get_offsets(), &[(0, 0)]].concat();
-                        let pair_special_tokens =
-                            [&vec![0u32; encoding.get_type_ids().len()][..], &[1]].concat();
-                        let pair_attention_mask = vec![1; pair_ids.len()];
+                                // For compatibility with `TemplateProcessing`, the sequence_ranges
+                                // shouldn't contain the special tokens.
+                                let pair_sequence_ranges =
+                                    HashMap::from_iter(vec![(1, 0..pair_ids.len() - 1)]);
+                                Encoding::new(
+                                    pair_ids,
+                                    pair_type_ids,
+                                    pair_tokens,
+                                    pair_words,
+                                    pair_offsets,
+                                    pair_special_tokens,
+                                    pair_attention_mask,
+                                    vec![],
+                                    pair_sequence_ranges,
+                                )
+                            })
+                            .collect(),
+                        pair_sequence_ranges,
+                    )
+                }
+            })
+            .collect();
 
-                        // For compatibility with `TemplateProcessing`, the sequence_ranges
-                        // shouldn't contain the special tokens.
-                        let pair_sequence_ranges =
-                            HashMap::from_iter(vec![(1, 0..pair_ids.len() - 1)]);
-                        Encoding::new(
-                            pair_ids,
-                            pair_type_ids,
-                            pair_tokens,
-                            pair_words,
-                            pair_offsets,
-                            pair_special_tokens,
-                            pair_attention_mask,
-                            vec![],
-                            pair_sequence_ranges,
-                        )
-                    })
-                    .collect(),
-                pair_sequence_ranges,
-            );
-
-            new_encoding.merge_with(new_pair_encoding, false);
-        }
-
-        Ok(vec![new_encoding])
+        Ok(encodings)
     }
 }
 
