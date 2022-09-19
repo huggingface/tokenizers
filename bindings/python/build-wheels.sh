908740873a3dd0065d491fa1920aa18d46230d57
Nicolas Patry <patry.nicolas@protonmail.com>
2022-09-19 16:30:36 +0200

    Local dir.

--- bindings/python/build-wheels.sh
+++ bindings/python/build-wheels.sh
@@ -4,26 +4,26 @@ set -ex
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
-for PYBIN in /opt/python/{cp37-cp37m,cp38-cp38,cp39-cp39,cp310-cp310}/bin; do
+for PYBIN in /opt/python/cp{37,38,39,310}/bin; do
     export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
 
     "${PYBIN}/pip" install -U setuptools-rust setuptools wheel
-    "${PYBIN}/pip" wheel /io/ -w /io/dist/ --no-deps
+    "${PYBIN}/pip" wheel ./dist/ -w ./dist/ --no-deps
     rm -rf build/*
 done
 
-for whl in /io/dist/*.whl; do
+for whl in ./dist/*.whl; do
     auditwheel repair "$whl" -w dist/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/cp{37,38,39,310}*/bin; do
-    "${PYBIN}/pip" install tokenizers -f /io/dist/
+    "${PYBIN}/pip" install tokenizers -f ./dist/
 done
 
 # Keep only manylinux wheels
-rm /io/*-linux_*
-cp /io/*.whl dist/
+rm ./dist/*-linux_*
+cp ./dist/*.whl dist/
 
 
 # Upload wheels
