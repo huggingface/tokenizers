142918213d116f23343a81969f9f9ed80fb37ee6
Arthur Zucker <arthur.zucker@gmail.com>
2022-12-23 12:32:20 +0000

    some styling

--- bindings/node/native/src/tasks/tokenizer.rs
+++ bindings/node/native/src/tasks/tokenizer.rs
@@ -102,20 +102,32 @@ impl Task for DecodeTask {
 
     fn perform(&self) -> Result<Self::Output, Self::Error> {
         match self {
-            DecodeTask::Single(worker, ids, skip_special_tokens, spaces_between_special_tokens) => worker
-                .tokenizer
-                .read()
-                .unwrap()
-                .decode(ids.to_vec(), *skip_special_tokens, *spaces_between_special_tokens)
-                .map_err(|e| format!("{}", e))
-                .map(DecodeOutput::Single),
-            DecodeTask::Batch(worker, ids, skip_special_tokens, spaces_between_special_tokens) => worker
-                .tokenizer
-                .read()
-                .unwrap()
-                .decode_batch(ids.to_vec(), *skip_special_tokens, *spaces_between_special_tokens)
-                .map_err(|e| format!("{}", e))
-                .map(DecodeOutput::Batch),
+            DecodeTask::Single(worker, ids, skip_special_tokens, spaces_between_special_tokens) => {
+                worker
+                    .tokenizer
+                    .read()
+                    .unwrap()
+                    .decode(
+                        ids.to_vec(),
+                        *skip_special_tokens,
+                        *spaces_between_special_tokens,
+                    )
+                    .map_err(|e| format!("{}", e))
+                    .map(DecodeOutput::Single)
+            }
+            DecodeTask::Batch(worker, ids, skip_special_tokens, spaces_between_special_tokens) => {
+                worker
+                    .tokenizer
+                    .read()
+                    .unwrap()
+                    .decode_batch(
+                        ids.to_vec(),
+                        *skip_special_tokens,
+                        *spaces_between_special_tokens,
+                    )
+                    .map_err(|e| format!("{}", e))
+                    .map(DecodeOutput::Batch)
+            }
         }
     }
 
