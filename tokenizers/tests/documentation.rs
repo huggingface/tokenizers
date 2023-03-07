4d008c335ce58f9050b4c7f50e5d8bc4c2cdec94
arthur.zucker@gmail.com <arthur.zucker@gmail.com>
2023-03-07 09:22:43 +0000

    add tests in rust

--- tokenizers/tests/documentation.rs
+++ tokenizers/tests/documentation.rs
@@ -459,3 +459,33 @@ fn pipeline_bert() -> tokenizers::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn spaces_between_special_tokens() -> tokenizers::Result<()>{
+    let mut bert_tokenizer = Tokenizer::from_file("data/bert-wiki.json")?;
+    bert_tokenizer.add_special_tokens( &
+        [
+            AddedToken::from("[ABC]", false),
+            AddedToken::from("[DEF]", false),
+            AddedToken::from("GHI IHG", false),
+        ]
+    );
+    let input_ids = bert_tokenizer.encode("[ABC][DEF][ABC]GHI IHG[DEF]", false)?;
+    let decoded_wo_spaces = bert_tokenizer.decode(input_ids.get_ids().to_vec(), true, false)?;
+    println!("{}", decoded_wo_spaces);
+    assert_eq!(decoded_wo_spaces, "[ABC][DEF][ABC]GHI IHG[DEF]");
+
+    let decoded_w_spaces = bert_tokenizer.decode(input_ids.get_ids().to_vec(), true, true)?;
+    println!("{}", decoded_w_spaces);
+    // "[ABC] [DEF] [ABC] GHI IHG [DEF]"
+    assert_ne!(decoded_wo_spaces, decoded_w_spaces);
+    assert_eq!(decoded_w_spaces, "[ABC] [DEF] [ABC] GHI IHG [DEF]");
+
+    let input_ids = bert_tokenizer.encode("Welcome to the 🤗 Tokenizers library.[SEP]Let's have fun.", false)?;
+    let decoded_wo_spaces = bert_tokenizer.decode(input_ids.get_ids().to_vec(), true, false)?;
+    println!("{}", decoded_wo_spaces);
+    assert_eq!(decoded_wo_spaces, "welcome to the tok ##eni ##zer ##s library .");
+
+    Ok(())
+
+}
