774c357a2e417ed4909b55fde2d1c54338ef6019
Morgan Funtowicz <funtowiczmo@gmail.com>
2023-05-16 22:16:42 +0200

    Please ...

--- bindings/node/native/src/tasks/tokenizer.rs
+++ bindings/node/native/src/tasks/tokenizer.rs
@@ -115,7 +115,7 @@ impl Task for DecodeTask {
                 .unwrap()
                 .decode_batch(
                     &ids.iter().map(|v| v.as_slice()).collect::<Vec<&[u32]>>(),
-                    *skip_special_tokens
+                    *skip_special_tokens,
                 )
                 .map_err(|e| format!("{}", e))
                 .map(DecodeOutput::Batch),
