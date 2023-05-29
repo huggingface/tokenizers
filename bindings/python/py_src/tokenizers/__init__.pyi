fbe82310b7728ee7cdb6f8b38fbc2388f9d95771
Chris Ha <hac541309@gmail.com>
2023-05-29 23:46:18 +0900

    Update __init__.pyi
    fixed __init__.pyi

--- bindings/python/py_src/tokenizers/__init__.pyi
+++ bindings/python/py_src/tokenizers/__init__.pyi
@@ -472,7 +472,7 @@ class NormalizedString:
 
         Args:
             pattern: Pattern:
-                A pattern used to match the string. Usually a string or a Regex
+                A pattern used to match the string. Usually a string or a a regex built with tokenizers.Regex
 
             content: str:
                 The content to be used as replacement
@@ -494,7 +494,7 @@ class NormalizedString:
 
         Args:
             pattern: Pattern:
-                A pattern used to split the string. Usually a string or a Regex
+                A pattern used to split the string. Usually a string or a a regex built with tokenizers.Regex
 
             behavior: SplitDelimiterBehavior:
                 The behavior to use when splitting.
