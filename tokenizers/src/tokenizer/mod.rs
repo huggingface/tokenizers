28c18dc1884c073a29ba1a645c8a2926a1ec7cc9
Anthony Platanios <e.a.platanios@gmail.com>
2023-09-19 17:29:16 -0700

    Fix.

--- tokenizers/src/tokenizer/mod.rs
+++ tokenizers/src/tokenizer/mod.rs
@@ -401,7 +401,7 @@ where
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Tokenizer(
-    pub  TokenizerImpl<
+    TokenizerImpl<
         ModelWrapper,
         NormalizerWrapper,
         PreTokenizerWrapper,
