ab0e4279b8aebebebd4feba63e1c588f2497a135
Arthur <arthur.zucker@gmail.com>
2023-11-14 15:16:39 +0100

    revert a change

--- tokenizers/src/tokenizer/mod.rs
+++ tokenizers/src/tokenizer/mod.rs
@@ -658,10 +658,6 @@ where
 
         final_vocab
     }
-    /// Get the added tokens encoder
-    pub fn get_added_tokens_encoder(&self) -> HashMap<String, u32> {
-        self.added_vocabulary.get_vocab().clone()
-    }
 
     /// Get the added tokens decoder
     pub fn get_added_tokens_decoder(&self) -> HashMap<u32, AddedToken> {
