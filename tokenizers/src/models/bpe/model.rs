574b6085371121ddae295222383ff2876b64e2f8
Arthur Zucker <arthur.zucker@gmail.com>
2024-04-15 16:28:05 +0200

    rust lint

--- tokenizers/src/models/bpe/model.rs
+++ tokenizers/src/models/bpe/model.rs
@@ -462,18 +462,16 @@ impl BPE {
     fn tokenize_with_cache(&self, sequence: &str) -> Result<Vec<Token>> {
         if let Some(ref hit) = self.cache.as_ref().and_then(|c| c.get(sequence)) {
             Ok(self.word_to_tokens(hit).collect())
-        } else if let Some(id) = self.vocab.get(sequence){
+        } else if let Some(id) = self.vocab.get(sequence) {
             if self.ignore_merges {
                 Ok(vec![Token::new(*id, sequence.to_string().clone(), (0, 0))])
-            }
-            else {
+            } else {
                 let word = self.merge_word(sequence)?;
                 let ret = self.word_to_tokens(&word).collect();
                 if let Some(ref cache) = self.cache {
                     cache.set(sequence.to_owned(), word);
                 }
-            Ok(ret)
-
+                Ok(ret)
             }
         } else {
             let word = self.merge_word(sequence)?;
