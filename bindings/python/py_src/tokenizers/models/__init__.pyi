75b249cb6291807748d4fe094e7bec26bb6302bf
Arthur Zucker <arthur.zucker@gmail.com>
2024-04-15 11:24:51 +0200

    clippy

--- bindings/python/py_src/tokenizers/models/__init__.pyi
+++ bindings/python/py_src/tokenizers/models/__init__.pyi
@@ -112,6 +112,9 @@ class BPE(Model):
 
         byte_fallback (:obj:`bool`, `optional`):
             Whether to use spm byte-fallback trick (defaults to False)
+
+        check_word_in_vocab (:obj:`bool`, `optional`):
+            Whether or not to match tokens with the vocab before using merges.
     """
     def __init__(
         self,
@@ -124,6 +127,7 @@ class BPE(Model):
         end_of_word_suffix=None,
         fuse_unk=None,
         byte_fallback=False,
+        check_word_in_vocab=False,
     ):
         pass
 
