5fb8a2320ce7f544f4910a2cfac90a8cca58f8c5
Nicolas Patry <patry.nicolas@protonmail.com>
2024-08-06 09:58:33 +0200

    Legacy test.

--- tokenizers/src/models/mod.rs
+++ tokenizers/src/models/mod.rs
@@ -252,7 +252,11 @@ mod tests {
             r#"{"type":"BPE","dropout":null,"unk_token":"<unk>","continuing_subword_prefix":null,"end_of_word_suffix":null,"fuse_unk":false,"byte_fallback":false,"ignore_merges":true,"vocab":{"<unk>":0,"a":1,"b":2,"ab":3},"merges":["a b"]}"#
         );
         let reconstructed = serde_json::from_str(&data).unwrap();
+        assert_eq!(model, reconstructed);
 
+        // Legacy check, type is not necessary.
+        let legacy = r#"{"dropout":null,"unk_token":"<unk>","continuing_subword_prefix":null,"end_of_word_suffix":null,"fuse_unk":false,"byte_fallback":false,"ignore_merges":true,"vocab":{"<unk>":0,"a":1,"b":2,"ab":3},"merges":["a b"]}"#;
+        let reconstructed = serde_json::from_str(&legacy).unwrap();
         assert_eq!(model, reconstructed);
 
         let invalid = r#"{"type":"BPE","dropout":null,"unk_token":"<unk>","continuing_subword_prefix":null,"end_of_word_suffix":null,"fuse_unk":false,"byte_fallback":false,"ignore_merges":true,"vocab":{"<unk>":0,"a":1,"b":2,"ab":3},"merges":["a b c"]}"#;
