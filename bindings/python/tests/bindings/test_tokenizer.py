61cfeb7fceb9d80bbedc0a61b034865d80577826
Nicolas Patry <patry.nicolas@protonmail.com>
2024-08-07 12:01:05 +0200

    Fixing tokenizer.

--- bindings/python/tests/bindings/test_tokenizer.py
+++ bindings/python/tests/bindings/test_tokenizer.py
@@ -555,8 +555,7 @@ class TestTokenizer:
 class TestTokenizerRepr:
     def test_repr(self):
         tokenizer = Tokenizer(BPE())
-        out = tokenizer.repr()
-        print(out)
+        out = repr(tokenizer)
         assert (
             out
             == 'Tokenizer(version="1.0", truncation=None, padding=None, added_tokens=[], normalizer=None, pre_tokenizer=None, post_processor=None, decoder=None, model=BPE(dropout=None, unk_token=None, continuing_subword_prefix=None, end_of_word_suffix=None, fuse_unk=False, byte_fallback=False, ignore_merges=False, vocab={}, merges=[]))'
@@ -571,8 +570,7 @@ class TestTokenizerRepr:
             special_tokens=[("[CLS]", 1), ("[SEP]", 0)],
         )
         tokenizer.normalizer = Sequence([Lowercase(), Strip()])
-        out = tokenizer.repr()
-        print(out)
+        out = repr(tokenizer)
         assert (
             out
             == 'Tokenizer(version="1.0", truncation=None, padding=None, added_tokens=[], normalizer=Sequence(normalizers=[Lowercase(), Strip(strip_left=True, strip_right=True)]), pre_tokenizer=ByteLevel(add_prefix_space=True, trim_offsets=True, use_regex=True), post_processor=TemplateProcessing(single=[SpecialToken(id="[CLS]", type_id=0), Sequence(id=A, type_id=0), SpecialToken(id="[SEP]", type_id=0)], pair=[SpecialToken(id="[CLS]", type_id=0), Sequence(id=A, type_id=0), SpecialToken(id="[SEP]", type_id=0), Sequence(id=B, type_id=1), SpecialToken(id="[SEP]", type_id=1)], special_tokens={"[CLS]":SpecialToken(id="[CLS]", ids=[1], tokens=["[CLS]"]), "[SEP]":SpecialToken(id="[SEP]", ids=[0], tokens=["[SEP]"])}), decoder=None, model=BPE(dropout=None, unk_token=None, continuing_subword_prefix=None, end_of_word_suffix=None, fuse_unk=False, byte_fallback=False, ignore_merges=False, vocab={}, merges=[]))'
