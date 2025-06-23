94de7bf13c1373c1c7ad5414eef5770a07f6a747
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2025-06-23 14:16:52 -0600

    don't warn about pytest-run-parallel markers if it isn't installed

--- bindings/python/conftest.py
+++ bindings/python/conftest.py
@@ -1,13 +1,25 @@
 import pytest
 
 
+try:
+    import pytest_run_parallel  # noqa:F401
+    PARALLEL_RUN_AVAILABLE = True
+except ImportError:
+    PARALLEL_RUN_AVAILABLE = False
+
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true", default=False, help="run slow tests")
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
-
+    # Don't warn for `pytest-run-parallel` markers if they're not available
+    breakpoint()
+    if not PARALLEL_RUN_AVAILABLE:
+        config.addinivalue_line(
+            "markers",
+            "thread_unsafe: mark the test function as single-threaded",
+        )
 
 def pytest_collection_modifyitems(config, items):
     if config.getoption("--runslow"):
