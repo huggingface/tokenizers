295beb2a2905b500a9b4f98e6387d12f36a02f11
Tobias Pitters <tobias.pitters@gmail.com>
2025-11-28 17:19:21 +0100

    update init.pyi through stubs.py

--- bindings/python/py_src/tokenizers/processors/__init__.pyi
+++ bindings/python/py_src/tokenizers/processors/__init__.pyi
@@ -100,8 +100,9 @@ class ByteLevel(PostProcessor):
             Whether to trim the whitespaces from the produced offsets.
 
         add_prefix_space (:obj:`bool`, `optional`, defaults to :obj:`True`):
-            Whether the add_prefix_space option was enabled during pre-tokenization. This
-            is relevant because it defines the way the offsets are trimmed out.
+            If :obj:`True`, keeps the first token's offset as is. If :obj:`False`, increments
+            the start of the first token's offset by 1. Only has an effect if :obj:`trim_offsets`
+            is set to :obj:`True`.
     """
     def __init__(self, trim_offsets=True, add_prefix_state=True):
         pass
