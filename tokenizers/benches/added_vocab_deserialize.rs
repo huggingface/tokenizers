6b80d6e4d93dd0010122f1e328b639aaedae42b2
Arthur <arthur.zucker@gmail.com>
2026-03-30 12:03:04 +0200

    bench: reduce normalizers to 2 (none, nfkc) for faster CI runs

--- tokenizers/benches/added_vocab_deserialize.rs
+++ tokenizers/benches/added_vocab_deserialize.rs
@@ -34,20 +34,8 @@ fn saved_tokenizer_path<N: Normalizer + Into<NormalizerWrapper>>(
 
 #[allow(clippy::type_complexity)]
 fn bench_deserialize(c: &mut Criterion) {
-    let normalizers: Vec<(&str, Option<fn() -> NormalizerWrapper>)> = vec![
-        ("none", None),
-        ("byte_level", Some(|| ByteLevel.into())),
-        ("lowercase", Some(|| Lowercase.into())),
-        ("nfc", Some(|| NFC.into())),
-        ("nfd", Some(|| NFD.into())),
-        ("nfkc", Some(|| NFKC.into())),
-        ("nfkd", Some(|| NFKD.into())),
-        ("nmt", Some(|| Nmt.into())),
-        ("strip", Some(|| Strip::new(true, true).into())),
-        ("replace", Some(|| Replace::new("a", "b").unwrap().into())),
-        ("prepend", Some(|| Prepend::new("pre_".to_string()).into())),
-        ("bert", Some(|| BertNormalizer::default().into())),
-    ];
+    let normalizers: Vec<(&str, Option<fn() -> NormalizerWrapper>)> =
+        vec![("none", None), ("nfkc", Some(|| NFKC.into()))];
 
     for &size in &[100_000, 400_000] {
         for (norm_name, maybe_factory) in &normalizers {
