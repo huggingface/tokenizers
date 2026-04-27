1dfd27a8eb2798bdb968e99db9cdde757f09391c
Arthur <48595927+ArthurZucker@users.noreply.github.com>
2026-04-27 09:53:57 +0200

    Delete bindings/python/build.rs

--- bindings/python/build.rs
+++
@@ -1,6 +0,0 @@
-// Re-emit pyo3's build cfgs (notably `Py_GIL_DISABLED` on free-threaded Python)
-// for this crate so we can `#[cfg(Py_GIL_DISABLED)]` in our own source. Cargo's
-// `rustc-cfg` directives don't propagate to dependents by default.
-fn main() {
-    pyo3_build_config::use_pyo3_cfgs();
-}
