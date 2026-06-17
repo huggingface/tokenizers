0f0ec1eef16340462a6c2dbb80204d609b08add8
SBrandeis <33657802+SBrandeis@users.noreply.github.com>
2026-06-17 19:47:59 +0200

    Add custom normalizer + inline None

--- crates/tk-encode/src/normalize/mod.rs
+++ crates/tk-encode/src/normalize/mod.rs
@@ -11,49 +11,40 @@ pub trait Normalizer: Send + Sync {
 }
 
 pub enum NormalizePlan {
-    Sequence(Box<[NormalizePlan]>),
+    Custom(Box<dyn Normalizer>),
     Lowercase(LowercaseNormalizer),
     NFD(NFDNormalizer),
     NFC(NFCNormalizer),
     NFKC(NFKCNormalizer),
     NFKD(NFKDNormalizer),
-    None(IdentityNormalizer),
+    Sequence(Box<[NormalizePlan]>),
+    None,
 }
 
 impl Normalizer for NormalizePlan {
     fn normalize<'a>(&mut self, string: Cow<'a, str>) -> Result<Cow<'a, str>> {
         match self {
+            NormalizePlan::Custom(normalizer) => normalizer.normalize(string),
             NormalizePlan::NFC(normalizer) => normalizer.normalize(string),
             NormalizePlan::NFD(normalizer) => normalizer.normalize(string),
             NormalizePlan::NFKC(normalizer) => normalizer.normalize(string),
             NormalizePlan::NFKD(normalizer) => normalizer.normalize(string),
             NormalizePlan::Lowercase(normalizer) => normalizer.normalize(string),
-            NormalizePlan::None(normalizer) => normalizer.normalize(string),
             NormalizePlan::Sequence(seq) => {
                 let mut string = string;
                 for member in seq {
                     string = member.normalize(string)?;
                 }
                 Ok(string)
-            }
+            },
+            // No normalization: pass the string as-is
+            NormalizePlan::None => Ok(string),
+
         }
     }
 }
 
-#[derive(Debug)]
-pub struct IdentityNormalizer {}
 
-impl IdentityNormalizer {
-    pub fn new() -> Self {
-        Self {}
-    }
-}
-
-impl Normalizer for IdentityNormalizer {
-    fn normalize<'a>(&mut self, _s: Cow<'a, str>) -> Result<Cow<'a, str>> {
-        Ok(_s)
-    }
-}
 
 #[derive(Debug)]
 pub struct LowercaseNormalizer {}
