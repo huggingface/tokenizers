edc3621e298e11bfe6be562e145e0019eb7392be
SBrandeis <33657802+SBrandeis@users.noreply.github.com>
2026-06-17 18:20:20 +0200

    normalizer trait

--- crates/tk-core/src/lib.rs
+++ crates/tk-core/src/lib.rs
@@ -5,3 +5,7 @@ mod vocab;
 
 pub type AddedTokens = Vec<(usize, usize)>;
 pub type Offsets = Vec<(usize, usize)>;
+
+pub trait Normalizer: Send + Sync {
+    fn normalize(&self, mut s: Cow<str>) -> Cow<str>;
+}
