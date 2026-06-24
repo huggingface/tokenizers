f56580b86b7433841d1f1d910bdd0d05343e52c9
Luc Georges <luc.sydney.georges@gmail.com>
2026-06-24 19:26:22 +0200

    feat: impl new pretok for bert
    Signed-off-by: Luc Georges <luc.sydney.georges@gmail.com>

--- tokenizers/tk-encode/src/pre_tokenizers/bert.rs
+++ tokenizers/tk-encode/src/pre_tokenizers/bert.rs
@@ -1,3 +1,4 @@
+use crate::pipeline;
 use crate::tokenizer::{PreTokenizedString, PreTokenizer, Result, SplitDelimiterBehavior};
 use crate::utils::macro_rules_attribute;
 use unicode_categories::UnicodeCategories;
@@ -17,13 +18,104 @@ impl PreTokenizer for BertPreTokenizer {
     }
 }
 
+#[derive(PartialEq, Eq)]
+enum CharType {
+    Whitespace,
+    Punctuation,
+    Other,
+}
+
+impl pipeline::PreTokenizer for BertPreTokenizer {
+    fn pre_tokenize(&self, text: &str, out: &mut Vec<pipeline::Split>) -> Result<()> {
+        let mut start = 0;
+        let mut prev_type = None;
+        for (i, c) in text.char_indices() {
+            let ct = if c.is_whitespace() {
+                CharType::Whitespace
+            } else if is_bert_punc(c) {
+                CharType::Punctuation
+            } else {
+                CharType::Other
+            };
+
+            if let Some(pt) = prev_type {
+                if pt != ct || ct == CharType::Punctuation {
+                    if pt != CharType::Whitespace {
+                        out.push(pipeline::Split { range: start..i });
+                    }
+                    start = i;
+                }
+            }
+            prev_type = Some(ct);
+        }
+        if let Some(pt) = prev_type {
+            if pt != CharType::Whitespace {
+                out.push(pipeline::Split {
+                    range: start..text.len(),
+                });
+            }
+        }
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::{NormalizedString, OffsetReferential, OffsetType};
+    use super::BertPreTokenizer;
+    use crate::{NormalizedString, OffsetReferential, OffsetType, PreTokenizedString};
+
+    #[test]
+    fn basic_new() {
+        use crate::pipeline::PreTokenizer;
+
+        let pretok = BertPreTokenizer;
+        let pretokenized: &str = "   Hey friend!     How are you?!?  ";
+        let mut ranges = Vec::new();
+        pretok.pre_tokenize(pretokenized, &mut ranges).unwrap();
+        let got = ranges.into_iter().map(|s| s.range).collect::<Vec<_>>();
+        assert_eq!(
+            got,
+            vec![
+                3..6,
+                7..13,
+                13..14,
+                19..22,
+                23..26,
+                27..30,
+                30..31,
+                31..32,
+                32..33,
+            ]
+        );
+    }
+
+    #[test]
+    fn chinese_chars_new() {
+        use crate::pipeline::PreTokenizer;
+
+        let mut n = NormalizedString::from("野口里佳 Noguchi Rika");
+        n.transform(
+            n.get().to_owned().chars().flat_map(|c| {
+                if (c as usize) > 0x4E00 {
+                    vec![(' ', 0), (c, 1), (' ', 1)]
+                } else {
+                    vec![(c, 0)]
+                }
+            }),
+            0,
+        );
+        let pretokenized: &str = n.get();
+        let pretok = BertPreTokenizer;
+        let mut ranges = Vec::new();
+        pretok.pre_tokenize(pretokenized, &mut ranges).unwrap();
+        let got = ranges.into_iter().map(|s| s.range).collect::<Vec<_>>();
+        assert_eq!(got, vec![1..4, 6..9, 11..14, 16..19, 21..28, 29..33]);
+    }
 
     #[test]
     fn basic() {
+        use crate::PreTokenizer;
+
         let pretok = BertPreTokenizer;
         let mut pretokenized: PreTokenizedString = "Hey friend!     How are you?!?".into();
         pretok.pre_tokenize(&mut pretokenized).unwrap();
@@ -49,6 +141,8 @@ mod tests {
 
     #[test]
     fn chinese_chars() {
+        use crate::PreTokenizer;
+
         let mut n = NormalizedString::from("野口里佳 Noguchi Rika");
         n.transform(
             n.get().to_owned().chars().flat_map(|c| {
