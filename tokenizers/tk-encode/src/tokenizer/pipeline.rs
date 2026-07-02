0b04a25ddc34050f81c5ccfc205e3792bcaca9bd
SBrandeis <33657802+SBrandeis@users.noreply.github.com>
2026-07-02 15:07:57 +0200

    inline loop + comments

--- tokenizers/tk-encode/src/tokenizer/pipeline.rs
+++ tokenizers/tk-encode/src/tokenizer/pipeline.rs
@@ -68,17 +68,40 @@ pub enum Segment<'a> {
     SpecialToken(u32),
 }
 
+/// This iterator wraps the logic to extract special tokens from the input text
+/// It yields [`Segment`] of the input text, in order
+///
+/// [`Segment`] can either be a chunk of text ([`Segment::Text`]) that needs to be tokenized by the
+/// model, or a [`Segment::SpecialToken`] holding the id (u32) of the special token
+/// 
+/// Usage:
+/// 
+/// ```rust
+/// let mut segment_iterator = SpecialSegmentIterator::new(input, pattern_matcher, false);
+/// 
+/// for segment in segment_iterator {
+///     match segment {
+///         Segment::SpecialToken(token_id) => println!("Segment is a special token!"),
+///         Segment::Text(chunk) => println!("Segment is a chunk of text!"),
+///     }
+/// } 
+/// ```
 pub struct SpecialSegmentIterator<'a, 'b, PatternMatcher: PipelinePatternMatcher> {
+    /// The chunk of text from which we want to extract special tokens
     input: &'a str,
+    /// Implementor of [`PipelinePatternMatcher`] - the engine to match special tokens
+    pattern_matcher: &'b PatternMatcher,
+    /// Whether the input is normalized
+    normalized: bool,
     offset: usize,
     pending: Option<u32>,
-    pattern_matcher: &'b PatternMatcher,
-    normalized: bool,
 }
 
 impl<'a, 'b, PatternMatcher: PipelinePatternMatcher>
     SpecialSegmentIterator<'a, 'b, PatternMatcher>
 {
+    /// Create a new iterator over [`Segment`] of the [`input`].
+    /// This iterator will yield [`Segment`] in order.
     fn new(input: &'a str, pattern_matcher: &'b PatternMatcher, normalized: bool) -> Self {
         Self {
             input,
@@ -95,6 +118,7 @@ impl<'a, 'b, PatternMatcher: PipelinePatternMatcher> Iterator
 {
     type Item = Segment<'a>;
 
+    /// Get the next segment of the input.
     fn next(&mut self) -> Option<Self::Item> {
         // take resets the pending option to None
         if let Some(special_token) = self.pending.take() {
@@ -112,7 +136,8 @@ impl<'a, 'b, PatternMatcher: PipelinePatternMatcher> Iterator
         {
             let before_token = &self.input[self.offset..self.offset + start];
             if !before_token.is_empty() {
-                // Store the special token to return in the next call
+                // The iterator returns segments in order: we need to return the chunk of text and then the special token.
+                // Store the special token to return in the next call and return a [`Segment::Text`]
                 self.pending = Some(token);
                 self.offset += end;
                 return Some(Segment::Text(before_token));
@@ -165,50 +190,48 @@ impl PipelineTokenizer {
         let mut output: Vec<PipelineToken> = vec![];
         let mut pre_tokens: Vec<Split> = vec![];
 
+        // First, we extract all special tokens from the non-normalized input
         for segment in SpecialSegmentIterator::new(input, &self.added_vocabulary, false) {
             match segment {
                 Segment::SpecialToken(token) => {
                     output.push(PipelineToken { id: token });
                 }
                 Segment::Text(chunk) => {
-                    self.tokenize_chunk(chunk, &mut pre_tokens, &mut output)?;
+                    // Normalize the text segment
+                    let mut normalized: NormalizedString = chunk.into();
+                    if let Some(normalizer) = &self.normalizer {
+                        normalizer.normalize(&mut normalized)?;
+                    }
+
+                    // Extract special tokens from the normalized input
+                    for segment in
+                        SpecialSegmentIterator::new(normalized.get(), &self.added_vocabulary, true)
+                    {
+                        match segment {
+                            Segment::SpecialToken(token) => {
+                                output.push(PipelineToken { id: token });
+                            }
+                            Segment::Text(normalized_chunk) => {
+                                // Pre-tokenize the chunk of normalized text
+                                pre_tokens.clear();
+                                self.pre_tokenizer
+                                    .pre_tokenize(normalized_chunk, &mut pre_tokens)?;
+
+                                // Tokenize each chunk
+                                for pre_token in pre_tokens.iter() {
+                                    output.extend(
+                                        self.model
+                                            .tokenize(&normalized_chunk[pre_token.range()])?
+                                            .into_iter()
+                                            .map(|Token { id, .. }| PipelineToken { id }),
+                                    );
+                                }
+                            }
+                        }
+                    }
                 }
             };
         }
         Ok(output)
-    }
-
-    fn tokenize_chunk(
-        &self,
-        chunk: &str,
-        pre_tokens: &mut Vec<Split>,
-        output: &mut Vec<PipelineToken>,
-    ) -> Result<()> {
-        // todo: no more NormalizedString
-        let mut normalized: NormalizedString = chunk.into();
-        if let Some(normalizer) = &self.normalizer {
-            normalizer.normalize(&mut normalized)?;
-        }
-        for segment in SpecialSegmentIterator::new(normalized.get(), &self.added_vocabulary, true) {
-            match segment {
-                Segment::SpecialToken(token) => {
-                    output.push(PipelineToken { id: token });
-                }
-                Segment::Text(normalized_chunk) => {
-                    pre_tokens.clear();
-                    self.pre_tokenizer
-                        .pre_tokenize(normalized_chunk, pre_tokens)?;
-                    for pre_token in pre_tokens.iter() {
-                        output.extend(
-                            self.model
-                                .tokenize(&normalized_chunk[pre_token.range()])?
-                                .into_iter()
-                                .map(|Token { id, .. }| PipelineToken { id }),
-                        );
-                    }
-                }
-            }
-        }
-        Ok(())
     }
 }
