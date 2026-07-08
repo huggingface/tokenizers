468d1556c3a8137035f863c5895003d4c03cd1c1
SBrandeis <33657802+SBrandeis@users.noreply.github.com>
2026-07-08 11:58:44 +0200

    fix None pretokenizer

--- tokenizers/tk-encode/src/tokenizer/pipeline.rs
+++ tokenizers/tk-encode/src/tokenizer/pipeline.rs
@@ -72,7 +72,13 @@ pub enum PipelinePreTokenizer {
 impl PreTokenizer for PipelinePreTokenizer {
     fn pre_tokenize(&self, text: &str, out: &mut Vec<Split>) -> Result<()> {
         match self {
-            Self::None => Ok(()),
+            Self::None => {
+                out.push(Split {
+                    start: 0,
+                    end: text.len() as u32,
+                });
+                Ok(())
+            }
             Self::Bert(pretok) => pretok.pre_tokenize(text, out),
             Self::Delimiter(pretok) => pretok.pre_tokenize(text, out),
             Self::Digits(pretok) => pretok.pre_tokenize(text, out),
@@ -320,7 +326,7 @@ impl PipelineTokenizer {
                                 // Tokenize each chunk
                                 for pre_token in pre_tokens.iter() {
                                     self.model.tokenize_bytes(
-                                        &normalized_chunk[pre_token.range()].as_bytes(),
+                                        normalized_chunk[pre_token.range()].as_bytes(),
                                         &mut output,
                                     )?;
                                 }
