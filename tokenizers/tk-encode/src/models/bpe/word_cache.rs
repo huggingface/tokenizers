e5e841807dd9cb4ff859c8da31ba8505af741b80
SBrandeis <33657802+SBrandeis@users.noreply.github.com>
2026-07-21 16:00:11 +0200

    rewrite without hashmap

--- tokenizers/tk-encode/src/models/bpe/word_cache.rs
+++ tokenizers/tk-encode/src/models/bpe/word_cache.rs
@@ -1,29 +1,83 @@
-use ahash::AHashMap;
+use std::ops::Range;
 
+use ahash::RandomState;
 
-/// naive implem of word -> IDs cache
+use crate::utils::cache::MAX_LENGTH;
+
+#[derive(Clone, Copy, Default)]
+struct CacheSlot {
+    hash: u64,
+    key_offsets: (u32, u16),
+    ids_offsets: (u32, u16),
+}
+
+impl CacheSlot {
+    fn id_range(&self) -> Range<usize> {
+        let (start, len) = self.ids_offsets;
+        start as usize..(start as usize + len as usize)
+    }
+
+    fn key_range(&self) -> Range<usize> {
+        let (start, len) = self.key_offsets;
+        start as usize..(start as usize + len as usize)
+    }
+}
+
 pub struct WordCache {
-    capacity: usize,
-    lookup: AHashMap<String, Box<[u32]>>,
+    hasher: RandomState,
+    slots: Box<[CacheSlot]>,
+    key_bytes: Vec<u8>,
+    ids: Vec<u32>,
+    slot_mask: u64,
 }
 
 impl WordCache {
-    pub fn new(capacity: usize) -> Self {
+    pub fn new() -> Self {
+        // todo: make capacity configurable
+        const CAPACITY: usize = 1 << 16;
+        const MASK: u64 = (CAPACITY as u64) - 1;
         Self {
-            capacity,
-            lookup: AHashMap::with_capacity(capacity),
+            hasher: RandomState::new(),
+            slots: vec![CacheSlot::default(); CAPACITY].into_boxed_slice(),
+            ids: Vec::with_capacity(CAPACITY * MAX_LENGTH),
+            key_bytes: Vec::with_capacity(CAPACITY * MAX_LENGTH),
+            slot_mask: MASK,
         }
     }
 
-    pub fn get(&self, key: &str) -> Option<&[u32]> {
-        self.lookup.get(key).map(|bx| &bx[..])
+    pub fn get(&self, key: &[u8]) -> Option<&[u32]> {
+        let hash = self.hasher.hash_one(key);
+        let slot = self.slots[(hash & self.slot_mask) as usize];
+        if hash != slot.hash {
+            return None;
+        }
+        if key != &self.key_bytes[slot.key_range()] {
+            return None;
+        }
+        Some(&self.ids[slot.id_range()])
     }
 
-    pub fn insert(&mut self, k: String, v: Vec<u32>) {
-        if self.lookup.len() >= self.capacity {
-            // Pop an arbitrary entry
-            self.lookup.extract_if(|_, _| true).next();
+    pub fn insert(&mut self, key: &[u8], ids: &[u32]) {
+        if key.len() > MAX_LENGTH {
+            return;
         }
-        self.lookup.insert(k, v.into_boxed_slice());
+        let hash = self.hasher.hash_one(key);
+        let slot_idx = (hash & self.slot_mask) as usize;
+
+        if self.slots[slot_idx].hash != 0 {
+            // slot already taken: skip insert
+            // caveat: a key whose hash lower bits resolves to 0 never gets cached
+            return;
+        }
+        let key_offsets = (self.key_bytes.len() as u32, key.len() as u16);
+        self.key_bytes.extend_from_slice(key);
+        let ids_offsets = (self.ids.len() as u32, ids.len() as u16);
+        self.ids.extend_from_slice(ids);
+
+        self.slots[slot_idx] = CacheSlot {
+            hash,
+            key_offsets,
+            ids_offsets,
+        };
     }
 }
