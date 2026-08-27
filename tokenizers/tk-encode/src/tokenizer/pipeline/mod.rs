435a50e2cb0228ac5610c92ee54611f77af652de
SBrandeis <33657802+SBrandeis@users.noreply.github.com>
2026-08-27 16:43:15 +0200

    truncation serializing

--- tokenizers/tk-encode/src/tokenizer/pipeline/mod.rs
+++ tokenizers/tk-encode/src/tokenizer/pipeline/mod.rs
@@ -566,6 +566,10 @@ impl PipelineTokenizer {
         self.inner.padding.as_ref()
     }
 
+    pub fn get_truncation(&self) -> Option<&TruncationParams> {
+        self.inner.truncation.as_ref()
+    }
+
     /// Encode `input` into token ids.
     ///
     /// Special tokens are matched in two passes:
--- tokenizers/tk-serialize/src/to_json/mod.rs
+++ tokenizers/tk-serialize/src/to_json/mod.rs
@@ -13,6 +13,7 @@ mod normalizers;
 mod padding;
 mod post_processors;
 mod pre_tokenizers;
+mod truncation;
 mod writer;
 
 #[cfg(all(test, feature = "deserialize"))]
@@ -25,6 +26,7 @@ use self::normalizers::write_normalizer;
 use self::padding::write_padding;
 use self::post_processors::write_post_processor;
 use self::pre_tokenizers::write_pre_tokenizer;
+use self::truncation::write_truncation;
 use self::writer::Out;
 use tk_encode::pipeline::PipelineTokenizer;
 use tk_encode::tokenizer::Result;
@@ -49,8 +51,8 @@ pub fn to_json(tokenizer: &PipelineTokenizer) -> Result<String> {
     let normalizers = tokenizer.get_normalizers();
     out.obj_open();
     out.field_str("version", "2.0");
-    // TODO: this is REQUIRED for v1
-    out.field_null("truncation");
+    out.key("truncation");
+    write_truncation(&mut out, tokenizer.get_truncation());
     out.key("padding");
     write_padding(&mut out, tokenizer.get_padding());
     // Which token plays which role, so this file can stand in for a `tokenizer_config.json`.
