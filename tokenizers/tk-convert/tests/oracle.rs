d4877b6867ebab9b9ba484f0d0f6905b8e7f23c8
SBrandeis <33657802+SBrandeis@users.noreply.github.com>
2026-09-11 10:02:31 +0200

    skip oracle tests that require truncatin

--- tokenizers/tk-convert/tests/oracle.rs
+++ tokenizers/tk-convert/tests/oracle.rs
@@ -186,6 +186,7 @@ fn llama_3_2_1b() {
 }
 
 #[test]
+#[ignore] // TODO: un-skip this once we ship truncation
 fn all_minilm_l6_v2() {
     assert_matches_released(
         "sentence-transformers/all-MiniLM-L6-v2",
@@ -194,6 +195,7 @@ fn all_minilm_l6_v2() {
 }
 
 #[test]
+#[ignore] // TODO: un-skip this once we ship truncation
 fn all_mpnet_base_v2() {
     assert_matches_released(
         "sentence-transformers/all-mpnet-base-v2",
