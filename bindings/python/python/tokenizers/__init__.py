6733cdc996048b71afcbbabd2c747ab97f479d7a
SBrandeis <33657802+SBrandeis@users.noreply.github.com>
2026-09-15 17:20:45 +0200

    lint

--- bindings/python/python/tokenizers/__init__.py
+++ bindings/python/python/tokenizers/__init__.py
@@ -7,4 +7,3 @@ Encode text to token ids and decode token ids back to text
 from .tokenizers import Encoding, Padding, Tokenizer, __version__
 
 __all__ = ["Encoding", "Padding", "Tokenizer", "__version__"]
-
